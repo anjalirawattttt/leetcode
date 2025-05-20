62d415c2b7abeec1b34c32a7cac50cce4a20f154
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-05-20 10:37:38 +0530

    Time: 0 ms (100%), Space: 11.9 MB (8.18%) - LeetHub

---
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flattenDFS(Node* node){
+        Node* curr=node;
+        Node* last=NULL;
+
+        while(curr){
+            Node* next=curr->next;
+            if(curr->child){
+                Node* child=curr->child;
+                Node* childTail;
+
+                childTail=flattenDFS(child);
+
+                curr->next=child;
+                child->prev=curr;
+
+                if(next){
+                    childTail->next=next;
+                    next->prev=childTail;
+                }
+                curr->child=NULL;
+                last=childTail;
+            }
+            else{
+                last=curr;
+            }
+            curr=next;
+        }
+        return last;
+        
+    }
+
+    Node* flatten(Node* head) {
+        if(!head)return head;
+        flattenDFS(head);
+        return head;
+    }
+};
