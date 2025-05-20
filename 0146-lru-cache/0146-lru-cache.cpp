bec09f17cba4c0b8161dc6024efef977c32700fe
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-05-20 14:03:33 +0530

    Time: 91 ms (48.49%), Space: 178.9 MB (47.16%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,72 @@
+class LRUCache {
+public:
+    typedef struct Node{
+        int key;
+        int value;
+        Node* prev;
+        Node* next;
+        Node(int k,int v){
+            key=k;
+            value=v;
+            prev=NULL;
+            next=NULL;
+        }
+    }Node;
+
+    int capacity;
+    unordered_map<int,Node*> m;
+    //dummy
+    Node* head;
+    Node* tail;
+
+    LRUCache(int capacity) {
+        this->capacity=capacity;
+        head=new Node(0,0);
+        tail=new Node(0,0);
+        head->next=tail;
+        tail->prev=head;
+    }
+
+    void remove(Node* node){
+        node->prev->next=node->next;
+        node->next->prev=node->prev;
+    }
+
+    void insert(Node* node){
+        node->prev=head;
+        node->next=head->next;
+        head->next->prev=node;
+        head->next=node;
+    }
+    
+    int get(int key) {
+        if(m.find(key)==m.end())return -1;
+        Node* node=m[key];
+        remove(node);
+        insert(node);
+        return node->value;
+    }
+    
+    void put(int key, int value) {
+        if(m.find(key)!=m.end()){
+            remove(m[key]);
+            delete(m[key]);
+        }
+        Node* node=new Node(key,value);
+        insert(node);
+        m[key]=node;
+        if(m.size()>capacity){
+            Node* lru=tail->prev;
+            remove(lru);
+            m.erase(lru->key);
+            delete lru;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
