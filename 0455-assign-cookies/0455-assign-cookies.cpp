ef372f537efcc06d191d722339e7e75fdecf1771
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-05-23 16:28:07 +0530

    Time: 17 ms (9.1%), Space: 44.9 MB (43.29%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -4,21 +4,20 @@ public:
         int ans=0;
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());      
-        int j=0;
-        for(int i=0;i<g.size();i++){
-            if(j>=s.size())break;
+        int i=0,j=0;
+        while(j<s.size() && i<g.size()){
             if(g[i]<=s[j]){
                 ans++;
                 j++;
+                i++;
             }
             else{
                 while(j<s.size() && g[i]>s[j]){
                     j++;
                 }
-                i--;
             }
         }
-
+        
         
 
         return ans;
