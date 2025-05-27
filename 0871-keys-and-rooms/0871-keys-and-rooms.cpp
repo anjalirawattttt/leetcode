3b6eab8b2dc4d321de5a7fb20097b3d2a733afc1
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-05-27 12:24:36 +0530

    Time: 0 ms (100%), Space: 14.4 MB (88.83%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -3,6 +3,7 @@ public:
     void check(vector<vector<int>>& rooms,vector<bool> &visited,int &count,int s){
         count++;
         visited[s]=true;
+        if(count==rooms.size())return;
         for(int &v:rooms[s]){
             if(visited[v]==false){
                 check(rooms,visited,count,v);   
@@ -14,7 +15,7 @@ public:
         int n=rooms.size();
         int count=0;
         vector<bool> visited(n,false);
-         check(rooms,visited,count,0);
+        check(rooms,visited,count,0);
         if(count==n)return true;
         return false;   
     }
