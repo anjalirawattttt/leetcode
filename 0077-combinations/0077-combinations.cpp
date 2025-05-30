a33f811ee8858b30b7d303d69fa2a54998b3cdb5
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-05-30 08:57:30 +0530

    Time: 1015 ms (5.01%), Space: 649.3 MB (5.07%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void helper(int n,int k,vector<vector<int>>& ans,vector<int> subset,int pos){
+        if(k==0){
+            ans.push_back(subset);
+            return;                
+        }
+        if(pos>n){
+            // if(k==0){
+            //     ans.push_back(subset);                
+            // } 
+            return;   
+        }
+        if(n-pos+1 < k){
+            return;
+        }
+        helper(n,k,ans,subset,pos+1);
+        subset.push_back(pos);
+        helper(n,k-1,ans,subset,pos+1);
+        subset.pop_back();
+
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        helper(n,k,ans,{},1);
+        return ans;   
+    }
+};
