549ef038c26e4981b277acf3b6de6bf9dbe770ac
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-06-29 00:53:27 +0530

    Time: 1 ms (4.9%), Space: 8.8 MB (13.03%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,22 +3,17 @@ public:
     double myPow(double x,long long N){
 
         if(N==0)return 1.00000;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+
         double temp=pow(x,N/2);
-        if(N>0){
-            if(N%2==0){
-                return temp*temp;
-            }
-            else{
-                return temp*temp*x;
-            }
+        if(N%2==0){
+            return temp*temp;
         }
         else{
-            if(N%2==0){
-                return temp*temp;
-            }
-            else{
-                return temp*temp*(1/x);
-            }
+            return temp*temp*x;
         }
 
     }
