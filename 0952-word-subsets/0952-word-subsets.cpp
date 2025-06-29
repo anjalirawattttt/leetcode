dc1a9fcf4aa4120597b9698ad62ab4ed1749b01c
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-06-29 15:56:44 +0530

    Time: 46 ms (70.86%), Space: 106.8 MB (45.27%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> res;
+        vector<int> maxFreq(26,0);
+        for(const string &word:words2){
+            vector<int> freq(26,0);
+            for( const char &ch:word){
+                freq[ch-'a']++;
+            }
+            for(int i=0;i<26;i++){
+                maxFreq[i]=max(maxFreq[i],freq[i]);
+            }
+        } 
+
+        for(const string &word:words1){
+            bool flag=true;
+            vector<int> freq(26,0);
+            for( const char &ch:word){
+                freq[ch-'a']++;
+            }
+
+            for(int i=0;i<26;i++){
+                if(maxFreq[i]>freq[i]){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag)res.push_back(word);
+        }  
+
+        return res;
+
+    }
+};
