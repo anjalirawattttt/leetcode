f17608490e4dadc9a079558141c29b6d44ad8ce7
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-06-29 13:41:19 +0530

    Time: 0 ms (100%), Space: 10.4 MB (85.34%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    void helper(vector<vector<int>> &ans,vector<int>& nums,int i,vector<int> subset){
-        ans.push_back(subset);                
-            
+    void helper(vector<int>& nums,vector<vector<int>> &res,vector<int>& temp,int i){
+        res.push_back(temp);
+        if(i==nums.size()){
+            return;
+        }
+        
+        
         for(int j=i;j<nums.size();j++){
             if(j>i && nums[j]==nums[j-1])continue;
-            subset.push_back(nums[j]);
-            helper(ans,nums,j+1,subset);
-            subset.pop_back();
+            temp.push_back(nums[j]);
+            helper(nums,res,temp,j+1);
+            temp.pop_back();
         }
     }
+    
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
+        vector<vector<int>> res;
+        vector<int> temp;
         sort(nums.begin(),nums.end());
-        helper(ans,nums,0,{});
-        return ans;   
+        helper(nums,res,temp,0);
+        return res;  
     }
 };
