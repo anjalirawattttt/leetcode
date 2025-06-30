f308e4e134ab04da2b8bb1fb3d1c56a6242e4d5b
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-06-30 12:11:49 +0530

    Time: 4 ms (26.58%), Space: 14.1 MB (60.56%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    void helper(vector<vector<int>>& ans,vector<int>& candidates,int target,vector<int>& subset,int pos){
-        if(target==0){
-            ans.push_back(subset);
+    void helper(vector<vector<int>>& res,vector<int>& temp,vector<int>& candidates,int target,int i,int sum){
+        if(sum==target){
+            res.push_back(temp);
             return;
         }
-        if(target<0)return;
-        if(pos>=candidates.size())return;
-
-        
-        subset.push_back(candidates[pos]);
-        helper(ans,candidates,target-candidates[pos],subset,pos+1);
-        subset.pop_back();
-        while(pos+1<candidates.size() && candidates[pos]==candidates[pos+1])pos++;
-        helper(ans,candidates,target,subset,pos+1);
-
+        if(i==candidates.size() || sum>target)return;
+        temp.push_back(candidates[i]);
+        helper(res,temp,candidates,target,i+1,sum+candidates[i]);
+        temp.pop_back();
+        while(i+1<candidates.size() && candidates[i]==candidates[i+1])i++;
+        helper(res,temp,candidates,target,i+1,sum);
     }
+   
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-     vector<vector<int>> ans;
-     vector<int> subset;
-     sort(candidates.begin(),candidates.end());
-     helper(ans,candidates,target,subset,0);
-     sort(ans.begin(),ans.end());
-     return ans;   
+        vector<vector<int>> res;
+        vector<int> temp;
+        sort(candidates.begin(),candidates.end());
+        helper(res,temp,candidates,target,0,0);
+        return res;
+
     }
 };
