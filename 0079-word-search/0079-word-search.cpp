a6b71929386cf13e2c88531944930ae25a40087b
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-07-05 11:53:38 +0530

    Time: 891 ms (13.57%), Space: 11.4 MB (54.16%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    bool isSafe(int r,int c,int m,int n){
-        return r<m && c<n && r>=0 && c>=0;
-    }
+    
 
     bool helper(vector<vector<char>>& board, string word,int i,int sr,int sc){
         if(i==word.length())return true;
-        vector<int> rows={1,-1,0,0};
-        vector<int> cols={0,0,1,-1};
-        for(int k=0;k<4;k++){
-            int r = sr + rows[k];
-            int c = sc + cols[k];
-            if(isSafe(r,c,board.size(),board[0].size()) && board[r][c]!='#' && board[r][c]==word[i]){
-                char ch=board[r][c];
-                board[r][c]='#';
-                if(helper(board,word,i+1,r,c))return true;
-                board[r][c]=ch;
-            }
-        }
-        return false;
+        if(sr<0 || sc<0 || sr==board.size() || sc==board[0].size() || board[sr][sc]!=word[i] || board[sr][sc]=='#')return false;
+        char ch=board[sr][sc];
+        board[sr][sc]='#';
+        bool op1=helper(board,word,i+1,sr+1,sc);
+        bool op2=helper(board,word,i+1,sr-1,sc);
+        bool op3=helper(board,word,i+1,sr,sc+1);
+        bool op4=helper(board,word,i+1,sr,sc-1);
+        board[sr][sc]=ch;
+        return op1|op2|op3|op4;
+
+        
     }
 
     bool exist(vector<vector<char>>& board, string word) {
@@ -28,10 +24,8 @@ public:
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++){
                 if(board[i][j]==word[0]){
-                    char ch=board[i][j];
-                    board[i][j]='#';
-                    if(helper(board,word,1,i,j))return true;
-                    board[i][j]=ch;
+                    
+                    if(helper(board,word,0,i,j))return true;
                 }
             }
         }
