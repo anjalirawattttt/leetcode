50f2949a156a684f30b6d77bd50c17f976cc0e50
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-07-18 10:29:52 +0530

    Time: 1 ms (9.35%), Space: 10.7 MB (22.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int help(int n,vector<int>& nums,vector<int>& dp){
-        if(n==0)return nums[n];
-        if(n<0)return 0;
-        if(dp[n]!=-1)return dp[n];
-        //pick
-        int op1=nums[n]+help(n-2,nums,dp);
-        //not pick
-        int op2=help(n-1,nums,dp);
-        return dp[n]=max(op1,op2);
-    }
+
+    // int help(int n,vector<int>& nums,vector<int>& dp){
+    //     if(n==0)return nums[n];
+    //     if(n<0)return 0;
+    //     if(dp[n]!=-1)return dp[n];
+    //     //pick
+    //     int op1=nums[n]+help(n-2,nums,dp);
+    //     //not pick
+    //     int op2=help(n-1,nums,dp);
+    //     return dp[n]=max(op1,op2);
+    // }
+
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n,-1);
-        return help(n-1,nums,dp);    
+        dp[0]=nums[0];
+        int neg=0;
+        for(int i=1;i<n;i++){
+            int op1=INT_MAX;
+            if(i==1)op1=nums[i]+neg;
+            else op1=nums[i]+dp[i-2];
+            int op2=dp[i-1];
+            dp[i]=max(op1,op2);
+        }
+        return dp[n-1];    
     }
+
 };
