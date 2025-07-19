94594e70c69a674edc2dea2c0cfcf6989c61c42a
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-07-20 00:34:27 +0530

    Time: 0 ms (100%), Space: 13.4 MB (5.94%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -8,24 +8,19 @@ public:
             dp.push_back(vector<int> (i+1,-1));
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<triangle[i].size();j++){
-                if(i==0 && j==0){
+
+
+        for(int i=m-1;i>=0;i--){
+            for(int j=triangle[i].size()-1;j>=0;j--){
+                if(i==m-1){
                     dp[i][j]=triangle[i][j];
                 }
                 else{
-                    int top=INT_MAX,tl=INT_MAX;
-                    if(i>0 && j>0)tl=dp[i-1][j-1];
-                    if(i>0 && j<i)top=dp[i-1][j];
-                    dp[i][j]=triangle[i][j]+min(top,tl);
+                    dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
                 }
             }
         }
 
-        int ans=INT_MAX;
-        for(int i=0;i<dp[m-1].size();i++){
-            ans=min(ans,dp[m-1][i]);
-        }
-        return ans;
+        return dp[0][0];
     }
 };
