12470417b404c5f4b12874f38e8beaae6df280f7
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-07-22 21:11:10 +0530

    Time: 405 ms (24.44%), Space: 11.5 MB (91.84%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int help(int i,vector<int>& nums, int target){
+        if(i==0){
+            if(nums[0]==0 && target==0)return 2;
+            if(nums[0]==abs(target))return 1;
+            return 0;
+        }
+        //+
+        int op1=help(i-1,nums,target-nums[i]);
+        //-
+        int op2=help(i-1,nums,target+nums[i]);
+        return op1+op2;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        return help(n-1,nums,target);
+    }
+};
