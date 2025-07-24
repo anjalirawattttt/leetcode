82216f6487484e3109914196ead20c0fd0f96e94
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-07-24 13:11:37 +0530

    Time: 27 ms (55.46%), Space: 27.3 MB (67.64%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
     // int help(int i1,int i2,string text1, string text2,vector<vector<int>>& dp){
-    //     if(i1<0 || i2<0)return 0;
+    //     if(i1<0 || i2<0 )return 0;
     //     if(dp[i1][i2]!=-1)return dp[i1][i2];
-    //     if(text1[i1]==text2[i2]){
-    //         return dp[i1][i2]=1 + help(i1-1,i2-1,text1,text2,dp);
-    //     }
+    //     //match
+    //     if(text1[i1]==text2[i2])return dp[i1][i2]=1+help(i1-1,i2-1,text1,text2,dp);
+    //     //not match
     //     return dp[i1][i2]=max(help(i1-1,i2,text1,text2,dp),help(i1,i2-1,text1,text2,dp));
-
     // }
+    
     int longestCommonSubsequence(string text1, string text2) {
         int n1=text1.length();
         int n2=text2.length();
-        vector<int> prev(n2,0);
-        // if(text1[0] == text2[0]) prev[0] = 1;
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
+        for(int i=0;i<=n1;i++)dp[i][0]=0;
+        for(int i=0;i<=n2;i++)dp[0][i]=0;
 
-        for(int i=0;i<n2;i++){
-            if(text1[0] == text2[i]) prev[i] = 1;
-            else if(i > 0) prev[i] = prev[i-1];
-        }
-
-        
-
-        for(int i1=1;i1<n1;i1++){
-            vector<int> curr(n2,0);
-            if(text1[i1]==text2[0])curr[0]=1;
-            else curr[0]=prev[0];
-            for(int i2=1;i2<n2;i2++){
-                if(text1[i1]==text2[i2]){
-                    curr[i2]=1 + prev[i2-1];
-                }
+        for(int i1=1;i1<=n1;i1++){
+            for(int i2=1;i2<=n2;i2++){
+                if(text1[i1-1]==text2[i2-1]) dp[i1][i2]=1+dp[i1-1][i2-1];
+                //not match
                 else{
-                    curr[i2]=max(prev[i2],curr[i2-1]);   
+                    dp[i1][i2]=max(dp[i1-1][i2],dp[i1][i2-1]);
                 }
             }
-            prev=curr;
         }
-
-        return prev[n2-1];   
+        return dp[n1][n2];      
     }
 };
