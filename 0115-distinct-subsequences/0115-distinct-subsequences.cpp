3cb93f7b23e9d2422a74f9fc47dc89709f2f7182
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-01 13:19:21 +0530

    Time: 23 ms (78.02%), Space: 26 MB (77.32%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -17,20 +17,22 @@ public:
         if(m==0)return 1;
         if(n==0 || m>n)return 0;
         const int MOD = 1e9 + 7;
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<int> prev(m+1,0);
+        prev[0]=1;
         for(int i=1;i<=n;i++){
+            vector<int> curr(m+1,0);
+            curr[0]=1;
             for(int j=1;j<=m;j++){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%MOD;
+                    curr[j]=(prev[j-1]+prev[j])%MOD;
                 }
                 else{
-                    dp[i][j]=dp[i-1][j];
+                    curr[j]=prev[j];
                 }   
             }
+            prev=curr;
         }
-        return dp[n][m];   
+        return prev[m];   
     }
 };
