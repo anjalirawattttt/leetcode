7ba3e78fc21dc40a3633ecc5a0d65beea50b1ad8
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-01 22:54:11 +0530

    Time: 4 ms (20.46%), Space: 15.9 MB (71.42%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-    int help(int i,int buy,vector<int>& prices,vector<vector<int>>& dp){
-        if(i>=prices.size())return 0;
-        int profit=0;
-        if(dp[i][buy]!=-1)return dp[i][buy];
-        if(buy){
-            //buy-not buy
-            profit=max(-prices[i]+help(i+1,0,prices,dp),help(i+1,1,prices,dp));
-        }
-        else{
-            //sell-not sell
-            profit=max(+prices[i]+help(i+2,1,prices,dp),help(i+1,0,prices,dp));
-        }
-        return dp[i][buy]=profit;
-    }
+    // int help(int i,int buy,vector<int>& prices,vector<vector<int>>& dp){
+    //     if(i>=prices.size())return 0;
+    //     int profit=0;
+    //     if(dp[i][buy]!=-1)return dp[i][buy];
+    //     if(buy){
+    //         //buy-not buy
+    //         profit=max(-prices[i]+help(i+1,0,prices,dp),help(i+1,1,prices,dp));
+    //     }
+    //     else{
+    //         //sell-not sell
+    //         profit=max(+prices[i]+help(i+2,1,prices,dp),help(i+1,0,prices,dp));
+    //     }
+    //     return dp[i][buy]=profit;
+    // }
 
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
-        return help(0,1,prices,dp);
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                if(buy){
+                    //buy-not buy
+                    dp[i][buy]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                }
+                else{
+                    //sell-not sell
+                    dp[i][buy]=max(+prices[i]+dp[i+2][1],dp[i+1][0]);
+                } 
+            }
+        }
+        return dp[0][1];
         
     }
 };
