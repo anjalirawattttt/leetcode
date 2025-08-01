2f84701daa02dcff0be3495e6d897b2092f5d81e
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-01 17:59:46 +0530

    Time: 35 ms (42.88%), Space: 11.9 MB (86.55%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,52 +1,54 @@
 class Solution {
 public:
-    bool help(int i,int j,string s,string p,vector<vector<int>>& dp){
-        if(i<0 && j<0)return true;
-        if(j<0 && i>=0)return false;
-        if(i<0 && j>=0){
-            for(int k=0;k<=j;k++){
-                if(p[k]!='*')return false;
-            }
-            return true;
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
+    // bool help(int i,int j,string s,string p,vector<vector<int>>& dp){
+    //     if(i<0 && j<0)return true;
+    //     if(j<0 && i>=0)return false;
+    //     if(i<0 && j>=0){
+    //         for(int k=0;k<=j;k++){
+    //             if(p[k]!='*')return false;
+    //         }
+    //         return true;
+    //     }
+    //     if(dp[i][j]!=-1)return dp[i][j];
         
-        if(p[j]=='?' || s[i]==p[j]){
-            return dp[i][j]=help(i-1,j-1,s,p,dp);
-        } 
-        else if(p[j]=='*'){
-            return dp[i][j]=help(i,j-1,s,p,dp) || help(i-1,j,s,p,dp);
-        }
-        return dp[i][j]=false;
-    }
+    //     if(p[j]=='?' || s[i]==p[j]){
+    //         return dp[i][j]=help(i-1,j-1,s,p,dp);
+    //     } 
+    //     else if(p[j]=='*'){
+    //         return dp[i][j]=help(i,j-1,s,p,dp) || help(i-1,j,s,p,dp);
+    //     }
+    //     return dp[i][j]=false;
+    // }
     bool isMatch(string s, string p) {
         int n=s.length();
         int m=p.length();
-        
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
 
-        dp[0][0]=true;
+        vector<bool> prev(m+1,false);
+
+        prev[0]=true;
         for(int j=1;j<=m;j++){
-            dp[0][j]= true;
+            prev[j]= true;
             for(int k=1;k<=j;k++){
-                if(p[k-1]!='*')dp[0][j]= false;
+                if(p[k-1]!='*')prev[j]= false;
             }
         }
 
         for(int i=1;i<=n;i++){
+            vector<bool> curr(m+1,false);
             for(int j=1;j<=m;j++){
                 if(p[j-1]=='?' || s[i-1]==p[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=prev[j-1];
                 } 
                 else if(p[j-1]=='*'){
-                    dp[i][j]=dp[i][j-1] || dp[i-1][j];
+                    curr[j]=curr[j-1] || prev[j];
                 }
                 else{
-                    dp[i][j]=false;   
+                    curr[j]=false;   
                 }
             }
+            prev=curr;
         }
         
-        return dp[n][m];   
+        return prev[m];   
     }
 };
