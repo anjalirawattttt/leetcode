400cc2aa9b2a9ab870e011f1d14fd595e0a80c5d
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-01 13:17:36 +0530

    Time: 27 ms (70.6%), Space: 27 MB (63.39%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
-    int help(int i,int j,string s,string t,vector<vector<int>> &dp){
-        if(j<0)return 1;
-        if(i<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s[i]==t[j]){
-            return dp[i][j]=help(i-1,j-1,s,t,dp)+help(i-1,j,s,t,dp);
-        }
-        //else
-        return dp[i][j]=help(i-1,j,s,t,dp);
-    }
+    // int help(int i,int j,string s,string t,vector<vector<int>> &dp){
+    //     if(j<0)return 1;
+    //     if(i<0)return 0;
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     if(s[i]==t[j]){
+    //         return dp[i][j]=help(i-1,j-1,s,t,dp)+help(i-1,j,s,t,dp);
+    //     }
+    //     //else
+    //     return dp[i][j]=help(i-1,j,s,t,dp);
+    // }
+
     int numDistinct(string s, string t) {
         int n=s.length();
         int m=t.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return help(n-1,m-1,s,t,dp);    
+        if(m==0)return 1;
+        if(n==0 || m>n)return 0;
+        const int MOD = 1e9 + 7;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%MOD;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }   
+            }
+        }
+        return dp[n][m];   
     }
 };
