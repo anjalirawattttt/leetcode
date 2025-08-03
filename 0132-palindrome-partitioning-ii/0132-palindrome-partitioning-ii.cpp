2a51095e30356655db3f31f252307b76dc7a9dbf
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-03 21:23:36 +0530

    Time: 1270 ms (56.29%), Space: 9.5 MB (55.89%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -9,32 +9,22 @@ public:
         return true;
     }
 
-    // int help(int i,string s,vector<int>& dp){
-    //     if(i>=s.length())return 0;
-    //     if(dp[i]!=-1)return dp[i];
-    //     int minCuts=INT_MAX;
-    //     for(int j=i;j<s.length();j++){
-    //         if(isPalindrome(i,j,s)){
-    //             int cuts=1+help(j+1,s,dp);
-    //             minCuts=min(minCuts,cuts);
-    //         }
-    //     }
-    //     return dp[i]=minCuts;
-    // }
-
-    int minCut(string &s) {
-        int n=s.length();
-        vector<int> dp(n+1,0);
-        for(int i=n-1;i>=0;i--){
-            int minCuts=INT_MAX;
-            for(int j=n-1;j>=i;j--){
-                if(isPalindrome(i,j,s)){
-                    int cuts=1+dp[j+1];
-                    minCuts=min(minCuts,cuts);
-                }
+    int help(int i,string &s,vector<int>& dp){
+        if(i>=s.length())return 0;
+        if(dp[i]!=-1)return dp[i];
+        int minCuts=INT_MAX;
+        for(int j=i;j<s.length();j++){
+            if(isPalindrome(i,j,s)){
+                int cuts=1+help(j+1,s,dp);
+                minCuts=min(minCuts,cuts);
             }
-            dp[i]=minCuts;    
         }
-        return dp[0]-1;    
+        return dp[i]=minCuts;
+    }
+
+    int minCut(string s) {
+        int n=s.length();
+        vector<int> dp(n,-1);
+        return help(0,s,dp)-1;    
     }
 };
