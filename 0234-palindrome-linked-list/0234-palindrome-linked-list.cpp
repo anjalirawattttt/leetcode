dd491c54e6bc2ca9abdf18c153184dc747c3eed5
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-08 13:27:21 +0530

    Time: 58 ms (5.86%), Space: 125.7 MB (33.76%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,53 +10,23 @@
  */
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head){
-        if(!head || !head->next )return head;
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast && fast->next){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        return slow;
-    }
-    
-
-    ListNode* reverse(ListNode* head){
-        ListNode* prev=NULL,*curr =head,*next=NULL;
-        while(curr){
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-        }
-        return prev;
-    }
-
     bool isPalindrome(ListNode* head) {
-        if(!head || !head->next)return true;
-        //find middle
-        ListNode* middle=middleNode(head);
-        //separate LL
-        ListNode* head1=head;
-        ListNode* head2=middle;
-        ListNode* temp=head,*prev;
+        if(!head)return true;
+
+        string s="";
+        ListNode* temp=head;
         while(temp){
-            if(temp==middle){
-                prev->next=NULL;
-            }
-            prev=temp;
+            s+=to_string(temp->val);
             temp=temp->next;
         }
-        //reverse second LL
-        head2=reverse(head2);
-        //compare  
-            while(head1 && head2){
-                if(head1->val!=head2->val)return false;
-                head1=head1->next;
-                head2=head2->next;
-            }
-        
+
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s[i]!=s[j])return false;
+            i++;
+            j--;
+        }
         return true;
+
     }
 };
