65ecb60dad275b26dadb28c83d9089fb73fc5b1e
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-17 19:31:01 +0530

    Time: 298 ms (5.03%), Space: 54.5 MB (7.72%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,32 +1,60 @@
 class Solution {
 public:
-    void dfs(int i,int j,bool &flag,int &count,vector<vector<bool>> &visited,vector<vector<int>> &grid){
-        if(i==0 || j==0 || i==grid.size()-1 || j==grid[0].size()-1)flag=true;
-        visited[i][j]=true;
-        count++;
+    
+    
+
+
+
+
+
+    void dfs(int i,int j,vector<vector<int>>& grid){
+        grid[i][j]='S';
         vector<int> rows={1,-1,0,0};
         vector<int> cols={0,0,1,-1};
         for(int k=0;k<4;k++){
             int r=i+rows[k];
             int c=j+cols[k];
-            if(r>=0 && c>=0 && r<grid.size() && c<grid[0].size() && grid[r][c]==1 && !visited[r][c])dfs(r,c,flag,count,visited,grid);
+            if(r>=0 && c>=0 && r<grid.size() && c<grid[0].size() && grid[r][c]==1)dfs(r,c,grid);
         }
     }
+
+
+
+
+    
+
     int numEnclaves(vector<vector<int>>& grid) {
         int ans=0;
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        for(int i=0;i<m;i++){
+            //first col
+            if(grid[i][0]==1){
+                dfs(i,0,grid);
+            }
+            //last col
+            if(grid[i][n-1]==1){
+                dfs(i,n-1,grid);
+            }
+        }
+
+        for(int j=0;j<n;j++){
+            //first row
+            if(grid[0][j]==1){
+                dfs(0,j,grid);
+            }
+            //last row
+            if(grid[m-1][j]==1){
+                dfs(m-1,j,grid);
+            }
+        }
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1 && !visited[i][j]){
-                    bool flag=false;
-                    int count=0;
-                    dfs(i,j,flag,count,visited,grid);
-                    if(!flag)ans+=count;
-                }
+                if(grid[i][j]==1)ans++;
             }
-        }   
+        }
+
         return ans; 
     }
 };
