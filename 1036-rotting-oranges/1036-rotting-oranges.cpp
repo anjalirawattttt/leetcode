1683dad4692016c26a630ab0589a39b5b59416e4
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-17 13:22:43 +0530

    Time: 4 ms (16.21%), Space: 16.8 MB (75.23%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,40 +1,49 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>> q;
         int m=grid.size();
         int n=grid[0].size();
-        int fresh=0,time=0;
-        vector<int> rows={0,0,1,-1};
-        vector<int> cols={1,-1,0,0};
 
+        queue<pair<int,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2)q.push({i,j});
-                else if(grid[i][j]==1)fresh++;
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }
             }
-        }
+        } 
 
-        while(!q.empty() && fresh>0){
+        int time=0;
+        vector<int> rows={1,-1,0,0};
+        vector<int> cols={0,0,1,-1};
+
+        while(!q.empty()){
             int size=q.size();
-            
+            bool rot=false;
             while(size--){
-                auto [x,y]=q.front();
+                auto [r,c]=q.front();
                 q.pop();
-                for(int i=0;i<4;i++){
-                    int r=x+rows[i];
-                    int c=y+cols[i];
-                    if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==1){
-                        fresh--;
-                        grid[r][c]=2;
-                        q.push({r,c});
+                for(int k=0;k<4;k++){
+                    int nr=r+rows[k];
+                    int nc=c+cols[k];
+                    if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
+                        rot=true;
+                        grid[nr][nc]=2;
+                        q.push({nr,nc});
                     }
                 }
             }
-            time++;
+            if(rot)time++;
         }
 
-        return (fresh==0)?time:-1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
 
+        return time;
     }
 };
