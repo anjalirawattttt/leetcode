71fd662452ed8def153376399a94b5170101d8fd
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-19 12:43:47 +0530

    Time: 2 ms (83.58%), Space: 19 MB (94.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    bool dfs(vector<bool> &visited,int i,vector<int> &recStack,vector<vector<int>> &adj){
-        visited[i]=true;
-        recStack[i]=true;
-        for(int &u:adj[i]){
-            if(visited[u]==false){
-                if(!dfs(visited,u,recStack,adj))return false;
-            }
-            
-            else if(recStack[u]==true)return false;
-        }
-        recStack[i]=false;
-        return true;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> inDegrees(numCourses,0);
         vector<vector<int>> adj(numCourses);
-        vector<bool> visited(numCourses,false);
-        vector<int> recStack(numCourses,false);
 
-        for(int i=0;i<prerequisites.size();i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-        }  
+        for(auto &p:prerequisites){
+            adj[p[1]].push_back(p[0]);
+            inDegrees[p[0]]++;
+        }     
 
+        queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(visited[i]==false){
-                if(!dfs(visited,i,recStack,adj)) return false;;
+            if(inDegrees[i]==0)q.push(i);
+        }
+        int v=0;
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            v++;
+            for(int &v:adj[u]){
+                if(--inDegrees[v]==0)q.push(v);
             }
-        } 
-        return true;
+        }
+
+        return v==numCourses?true:false;
     }
 };
