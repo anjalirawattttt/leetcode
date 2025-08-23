23d3615dae6e6058706d542137ba75f5da3ff856
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-23 22:50:11 +0530

    Time: 0 ms (100%), Space: 87.3 MB (38.77%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int c5=0,c10=0;
-        for(int bill:bills){
-            if(bill==5){
+        int c5=0,c10=0,c20=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5){
                 c5++;
             }
-            else if(bill==10){
-                c10++;
-                if(c5>0){
+            else if(bills[i]==10){
+                if(c5){
                     c5--;
+                    c10++;
                 }
                 else{
                     return false;
                 }
             }
             else{
-                if(c5>0 && c10>0){
-                    c5--;
+                if(c10 && c5){
                     c10--;
+                    c5--;
+                    c20++;
                 }
-                else if(c5>2){
+                else if(c5>=3){
                     c5-=3;
+                    c20++;
                 }
                 else{
                     return false;
                 }
             }
-
-        }
-
-        return true;
+        } 
+        return true;   
     }
 };
