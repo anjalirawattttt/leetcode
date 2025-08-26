2875a2766e1fd31143a0b21a3a7ff1ce19676e10
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-26 13:50:43 +0530

    Time: 22 ms (22.16%), Space: 40.4 MB (7.96%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    int numSubarrayless(vector<int>& nums,int goal){
-        if(goal<0)return 0;
-        int l=0,r=0,n=nums.size();
-        int ans=0,sum=0;
-        while(r<n){
-            sum+=nums[r];
-            while(sum>goal){
-                sum-=nums[l];
-                l++;
-            }
-            if(sum<=goal){
-                ans+=r-l+1;
-            }
-
-            r++;
-        }
-        return ans;
-    }
-
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return numSubarrayless(nums,goal) - numSubarrayless(nums,goal-1);
+        int ans=0;
+        unordered_map<int,int> m;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum==goal)ans++;
+            ans+=m[ sum-goal ];
+            m[sum]++;
+        }  
+        return ans; 
     }
 };
