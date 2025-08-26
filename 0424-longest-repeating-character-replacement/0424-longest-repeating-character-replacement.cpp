1d252f69b5d89df7509627f8e437d85c620d2241
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-26 13:28:22 +0530

    Time: 2409 ms (5.12%), Space: 11.1 MB (18.73%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l=0,r=0,n=s.length();
-        int maxFreq=0,maxLength=0;
+        int maxLen=0;
         unordered_map<char,int> m;
+        int n=s.length();
+        int l=0,r=0,maxFreq=0; 
 
         while(r<n){
-            m[s[r]]++;
-            maxFreq=max(maxFreq,m[s[r]]);
-            int windowSize=r-l+1;
-            if(windowSize-maxFreq>k){
+            m[s[r]]++;  
+            if(m[s[r]]>maxFreq)maxFreq=m[s[r]];
+            if((r-l+1)-maxFreq>k){
                 m[s[l]]--;
                 l++;
+                maxFreq=0;
+                for(int i=l;i<=r;i++){
+                    maxFreq=max(maxFreq,m[s[i]]);   
+                }
             }
-
-            if(windowSize-maxFreq<=k){
-                maxLength=max(maxLength,r-l+1);
-            }
-
+            if((r-l+1)-maxFreq<=k)maxLen=max(maxLen,r-l+1); 
             r++;
         }
-        return maxLength;
+        return maxLen;  
     }
 };
