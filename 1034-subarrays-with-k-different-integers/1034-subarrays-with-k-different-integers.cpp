ea9895947f301f28781b550fcc5afad4e35f8aba
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-26 14:44:43 +0530

    Time: 67 ms (81.21%), Space: 53.3 MB (93.48%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int k){
-        if(k<0)return 0;
-        unordered_map<int,int> m;
+    int help(vector<int>& nums,int k){
         int l=0,r=0,n=nums.size();
-        int ans=0;
+        unordered_map<int,int> freq;
+        int ans=0,diff=0;
         while(r<n){
-            m[nums[r]]++;
-            while(m.size()>k){
-                m[nums[l]]--;
-                if(m[nums[l]]==0)m.erase(nums[l]);
-                l++;
+            if(freq[nums[r]]==0)diff++;
+            freq[nums[r]]++;
+            while(diff>k){
+                freq[nums[l]]--;
+                if(freq[nums[l]]==0)diff--;
+                l++;   
             }
-            if(m.size()<=k){
-                ans+=r-l+1;    
-            }
+            if(diff<=k)ans+=r-l+1;
             r++;
         }
         return ans;
     }
-
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return helper(nums,k) - helper(nums,k-1);    
+        return help(nums,k)-help(nums,k-1);   
     }
 };
