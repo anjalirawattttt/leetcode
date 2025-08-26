824cfd9d22e441483e86a194299d8bdb9e09861b
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-26 12:10:01 +0530

    Time: 7 ms (67.87%), Space: 11 MB (80.08%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int l=0,r=0,n=s.length(),ans=0;
-        int map[3];
-        fill(map,map+3,-1);
-
+        int ans=0;
+        vector<int> hash(3,-1);
+        int n=s.length();
+        int l=0,r=0;
         while(r<n){
-            map[s[r]-'a']=r;            
-            if(map[0]!=-1 && map[1]!=-1 && map[2]!=-1){
-                l=min(map[0],min(map[1],map[2]));
-                ans+=l+1;
-            }            
+            hash[s[r]-'a']=r;
+            if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1){
+                int minIdx=min(hash[0],min(hash[1],hash[2]));
+                ans+=minIdx+1;
+            }
             r++;
-        }  
-
-        return ans;     
+        }
+        return ans;    
     }
 };
