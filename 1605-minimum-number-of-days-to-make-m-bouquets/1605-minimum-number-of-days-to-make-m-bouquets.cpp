e03562c6aac728b84889555da7a583906542ebb8
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-29 10:00:59 +0530

    Time: 21 ms (65.71%), Space: 70.4 MB (76.12%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
-    int getDays(int lower,int upper,vector<int>& bloomDay,int m,int k){
-        int l=lower,r=upper,result=upper;
-        while(l<=r){
-            int cap=l+(r-l)/2;
-            int calB=0;
-            int count=0;
-
-            for(int b:bloomDay){
-                if(b<=cap){
-                    count++;
-                    if(count==k){
-                    calB++;
-                    count=0;
-                }
-                }
-                else{
-                    count=0;
-                }
-            }
-
-            if(calB>=m){
-                r=cap-1;
-            }
-            else{
-                l=cap+1;
-            }
-        }
-        return l;
-    }
-
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n=bloomDay.size();
-        if(1LL*m*k >n)return -1;
-        int lower=1,upper=bloomDay[0];
+        if((long long)m*k>n)return -1;
 
-        for(int &b:bloomDay){
-            upper=max(upper,b);
+        int maxDay=0;
+        for(int i=0;i<n;i++)maxDay=max(maxDay,bloomDay[i]);
+
+        int l=1,r=maxDay;
+        while(l!=r){
+            int mid=l+(r-l)/2;
+            int bouqets=0,flowers=0;
+
+            for(int i=0;i<n;i++){
+                if(bloomDay[i]<=mid){
+                    flowers++;
+                    if(flowers==k){
+                        bouqets++;
+                        flowers=0;
+                    }
+                }
+                else{
+                    flowers=0;
+                }
+            }
+
+            if(bouqets>=m)r=mid;
+            else l=mid+1;
         }
-
-        int days=getDays(lower,upper,bloomDay,m,k);
-        return days;
+        return l;
     }
 };
