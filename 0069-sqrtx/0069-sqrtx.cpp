9db027ea6576e631426bc6d08aefe4bd16683ffd
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-30 20:34:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.62%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0 || x<0 )return 0;
-        int l=1,r=x,mid;
-        int ans=0;
-
+        if(x==0 || x==1)return x;
+        int l=1,r=x/2;
+        int ans=1;
         while(l<=r){
-            mid=l + (r-l)/2;
-            long long s=1LL*mid*mid;
-            if(s==x)return mid;
-            else if(s<x){
+            int mid=l+(r-l)/2;
+            if((long long)mid*mid == x)return mid;
+            if((long long)mid*mid < x){
                 ans=mid;
                 l=mid+1;
             }
@@ -18,5 +16,6 @@ public:
             }
         }
         return ans;
+        
     }
 };
