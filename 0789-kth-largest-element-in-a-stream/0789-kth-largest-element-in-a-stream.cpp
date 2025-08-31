8eb81bf3f3cb0d7971087bbe543de0fe291418b1
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-01 00:41:30 +0530

    Time: 8 ms (50.32%), Space: 33.2 MB (6.49%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -1,24 +1,21 @@
 class KthLargest {
+    //minheap
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int x;
 public:
-    priority_queue<int,vector<int>,greater<int>> minHeap;
-    int size;
-
     KthLargest(int k, vector<int>& nums) {
-        size=k;
+        x=k;
         for(int i=0;i<nums.size();i++){
-            minHeap.push(nums[i]);
-            if(minHeap.size()>size){
-                minHeap.pop();
-            }
+            pq.push(nums[i]);
+            if(pq.size()>k)pq.pop();
         }
     }
     
     int add(int val) {
-        minHeap.push(val);
-        if(minHeap.size()>size){
-            minHeap.pop();
-        } 
-        return minHeap.top();  
+        pq.push(val);
+        if(pq.size()>x)pq.pop();     
+
+        return pq.top();
     }
 };
 
