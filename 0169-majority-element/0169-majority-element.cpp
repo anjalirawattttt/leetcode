9d588aab30eab704ab9407219c63c87b30a191ae
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-02 11:25:34 +0530

    Time: 0 ms (100%), Space: 28.3 MB (28.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        return nums[n/2];    
+        //moore voting algorithm
+        int count=0,candidate;
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                candidate=nums[i];
+                count++;
+            }
+            else{
+                if(nums[i]==candidate)count++;
+                else count--;
+            }
+        }
+        return candidate;    
     }
 };
