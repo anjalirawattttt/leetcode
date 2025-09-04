28d63f1a986bc90ca5a5689232b49fa7ea41e2c5
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-04 13:54:21 +0530

    Time: 0 ms (100%), Space: 12.6 MB (79.03%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1=nums1.size(),n2=nums2.size();
-        for(int i=0;i<n1;i++){
-            int x=nums1[i];
-            for(int j=0;j<n2;j++){
-                if(nums1[i]==nums2[j]){
-                    for(int k=j+1;k<n2;k++){
-                        if(nums2[k]>nums2[j]){
-                            x=nums2[k];
-                            break;
-                        }
-                    }
-                    nums1[i]= nums1[i]==x?-1:x;
+        unordered_map<int,int> m;
+        for(int i=0;i<n2;i++){
+            for(int j=i+1;j<n2;j++){
+                if(nums2[j]>nums2[i]){
+                    m[nums2[i]]=nums2[j];
                     break;
                 }
             }
+            m[nums2[i]]= m[nums2[i]]==0?-1:m[nums2[i]];
+        }
+
+        for(int i=0;i<n1;i++){
+            nums1[i]=m[nums1[i]];
         }
         return nums1;
     }
