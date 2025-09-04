826f7d17dc5b544aa5169b31b05a569c9a3b5c0b
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-04 13:39:13 +0530

    Time: 2 ms (93.18%), Space: 23.9 MB (30.93%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
+    static bool myCmp(vector<int> &a,vector<int> &b){
+        return a[0]<b[0];
+    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> v;
-        int n=intervals.size();
-        if(n<=1)return intervals;
-        sort(intervals.begin(),intervals.end(),[](const vector<int>& a,const vector<int>& b){
-            if(a[0]==b[0])return a[1]<b[1];
-            else return a[0]<b[0];
-        });
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end(),myCmp);   
 
-        int start=intervals[0][0];
-        int end=intervals[0][1];
-
-        for(int i=1;i<n;i++){
-            if(intervals[i][0]<=end){
-                end=max(end,intervals[i][1]);
+        int s=intervals[0][0],e=intervals[0][1];
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]>=s && intervals[i][0]<=e){
+                s=min(s,intervals[i][0]);
+                e=max(e,intervals[i][1]);
             }
             else{
-                v.push_back({start,end});
-                start=intervals[i][0];
-                end=intervals[i][1];
+                ans.push_back({s,e});
+                s=intervals[i][0];
+                e=intervals[i][1];
             }
         }
-        v.push_back({start,end});
-        return v;
+        ans.push_back({s,e});
+        return ans; 
     }
 };
