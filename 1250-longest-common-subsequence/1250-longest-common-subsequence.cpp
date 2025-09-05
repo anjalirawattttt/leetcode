341e6ad1428f9ede4164ac5162f6565dbad58ae3
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-05 13:23:13 +0530

    Time: 26 ms (64.41%), Space: 27.3 MB (69.09%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    // int help(int i1,int i2,string text1, string text2,vector<vector<int>>& dp){
-    //     if(i1<0 || i2<0 )return 0;
-    //     if(dp[i1][i2]!=-1)return dp[i1][i2];
-    //     //match
-    //     if(text1[i1]==text2[i2])return dp[i1][i2]=1+help(i1-1,i2-1,text1,text2,dp);
-    //     //not match
-    //     return dp[i1][i2]=max(help(i1-1,i2,text1,text2,dp),help(i1,i2-1,text1,text2,dp));
-    // }
-    
+    int help(string text1, string text2,int i1,int i2,vector<vector<int>> &dp){
+        if(i1<0 || i2<0)return 0;
+        if(dp[i1][i2]!=-1)return dp[i1][i2];
+        if(text1[i1]==text2[i2]){
+            return dp[i1][i2]=1+help(text1,text2,i1-1,i2-1,dp);
+        }
+        int op1=help(text1,text2,i1-1,i2,dp);
+        int op2=help(text1,text2,i1,i2-1,dp);
+        return dp[i1][i2]=max(op1,op2);
+    }
     int longestCommonSubsequence(string text1, string text2) {
-        int n1=text1.length();
-        int n2=text2.length();
-        vector<int> prev(n2+1,0);
-
+        int n1=text1.length(),n2=text2.length();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
         for(int i1=1;i1<=n1;i1++){
-            vector<int> curr(n2+1,0);
             for(int i2=1;i2<=n2;i2++){
-                if(text1[i1-1]==text2[i2-1]) curr[i2]=1+prev[i2-1];
-                //not match
+                if(text1[i1-1]==text2[i2-1]){
+                    dp[i1][i2]=1+dp[i1-1][i2-1];
+                }
                 else{
-                    curr[i2]=max(prev[i2],curr[i2-1]);
+                    int op1=dp[i1][i2-1];
+                    int op2=dp[i1-1][i2];
+                    dp[i1][i2]=max(op1,op2);
                 }
             }
-            prev=curr;
         }
-        return prev[n2];      
+        return dp[n1][n2];
     }
 };
