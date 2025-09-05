0b9d423cdfa07430d824fdf6e7a2fae0ba921845
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-05 09:35:06 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.6%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
+    int help(vector<int>& nums,int i,vector<int>& dp){
+        if(i<0)return 0;
+        if(i==0)return nums[i];
 
-    // int help(int n,vector<int>& nums,vector<int>& dp){
-    //     if(n==0)return nums[n];
-    //     if(n<0)return 0;
-    //     if(dp[n]!=-1)return dp[n];
-    //     //pick
-    //     int op1=nums[n]+help(n-2,nums,dp);
-    //     //not pick
-    //     int op2=help(n-1,nums,dp);
-    //     return dp[n]=max(op1,op2);
-    // }
-
+        if(dp[i]!=-1)return dp[i];
+        //rob
+        int op1=nums[i]+help(nums,i-2,dp);
+        //not rob
+        int op2=help(nums,i-1,dp);
+        return dp[i]=max(op1,op2);
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int prev=nums[0];
-        int prev2=0;
-        int curri=INT_MIN;
-        for(int i=1;i<n;i++){
-            int op1=nums[i]+prev2;
-            int op2=prev;
-            curri=max(op1,op2);
-            prev2=prev;
-            prev=curri;
-        }
-        return prev;    
+        vector<int> dp(n,-1);
+        return help(nums,n-1,dp);   
     }
-
 };
