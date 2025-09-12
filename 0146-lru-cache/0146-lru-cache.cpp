03c627f8e3b87d49a4d3444a60669d745d9e2981
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-12 19:14:37 +0530

    Time: 60 ms (86.69%), Space: 173 MB (82.72%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,64 +1,81 @@
 class LRUCache {
-public:
-    typedef struct Node{
-        int key,value;
-        Node* prev,*next;
+    struct Node{
+        int key,val;
+        Node *prev,*next;
         Node(int k,int v){
             key=k;
-            value=v;
-            prev=NULL;
-            next=NULL;
+            val=v;
+            prev=next=NULL;
         }
-
-    }Node;
-
-    int capacity;
+    };
     unordered_map<int,Node*> m;
-    Node* head;
-    Node* tail;
-
-    void insert(Node* node){
-        node->next=head->next;
-        node->prev=head;
-        head->next->prev=node;
-        head->next=node;
+    int size=0;
+    int cap;
+    Node* head=NULL;
+    Node* tail=NULL;
+public:
+    void insertAtBegining(Node* node){
+        if(!head){
+            head=node;
+            tail=node;
+            return;
+        }
+        head->prev=node;
+        node->next=head;
+        head=node;
     }
-
-    void remove(Node* node){
-        node->prev->next=node->next;
-        node->next->prev=node->prev;
+    void deleteAtEnd(){
+        if(head==tail){
+            Node* temp=head;
+            m.erase(temp->key);
+            head=NULL;
+            tail=NULL;
+            delete temp;
+            return;
+        }
+        Node* temp=tail;
+        m.erase(temp->key);
+        tail=tail->prev;
+        tail->next=NULL;
+        delete temp;
+    }
+    void moveToFront(Node* temp){
+        if(temp==head)return;
+        if(temp==tail)tail=tail->prev;
+        temp->prev->next=temp->next;
+        if(temp->next)temp->next->prev=temp->prev;
+        temp->next=head;
+        temp->prev=NULL;
+        head->prev=temp;
+        head=head->prev;
     }
 
     LRUCache(int capacity) {
-        this->capacity=capacity;
-        head=new Node(0,0);
-        tail=new Node(0,0);
-        head->next=tail;
-        tail->prev=head;
+        cap=capacity;
     }
     
     int get(int key) {
         if(m.find(key)==m.end())return -1;
-        Node* node=m[key];
-        remove(node);
-        insert(node);
-        return node->value;
+        Node* temp=m[key];
+        moveToFront(temp);
+        return temp->val;
     }
     
     void put(int key, int value) {
         if(m.find(key)!=m.end()){
-            remove(m[key]);
-            delete(m[key]);
+            Node* temp=m[key];
+            temp->val=value;
+            moveToFront(temp);
+            return;
         }
-
         Node* node=new Node(key,value);
-        insert(node);
         m[key]=node;
-        if(m.size()>capacity){
-            Node* lru=tail->prev;
-            remove(lru);
-            m.erase(lru->key);
-            delete lru;
+        insertAtBegining(node);
+        // head=node;
+        size++;
+        if(size>cap){
+            deleteAtEnd();
+            size--;
         }
     }
 };
