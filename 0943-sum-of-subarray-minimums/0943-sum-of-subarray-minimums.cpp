9af20dcf93b09699acc89a4b3ed19bb4179a0fe7
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-12 11:10:19 +0530

    Time: 19 ms (43.04%), Space: 47 MB (89.66%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    const int MOD = 1e9+7;
-
+    const int MOD=1e9+7;
     int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> left(n), right(n);
-        stack<int> st;
-
-        // Previous Less
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[st.top()] > arr[i]) st.pop();
-            left[i] = st.empty() ? i + 1 : i - st.top();
-            st.push(i);
+        int n=arr.size();
+        long long ans=0;
+        stack<int> s;
+        vector<int> left(n,1);
+        vector<int> right(n,1);
+        for(int i=0;i<n;i++){
+            while(!s.empty() && arr[s.top()]>arr[i])s.pop();
+            left[i]=s.empty() ? i+1 :i-s.top();
+            s.push(i);
         }
-
-        while (!st.empty()) st.pop();
-
-        // Next Less or Equal
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) st.pop();
-            right[i] = st.empty() ? n - i : st.top() - i;
-            st.push(i);
+        while(!s.empty())s.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && arr[s.top()]>=arr[i])s.pop();
+            right[i]=s.empty() ? n-i : s.top()-i;
+            s.push(i);
         }
-
-        long long ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = (ans + 1LL * arr[i] * left[i] * right[i]) % MOD;
+        for(int i=0;i<n;i++){
+            ans=(ans+(arr[i]*1LL*left[i]*right[i])%MOD)%MOD;
         }
-
-        return (int)ans;
+        return (int)ans;    
     }
 };
