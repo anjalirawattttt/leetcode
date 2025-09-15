8a6368597b9bd5537b8d0afc6026ce62203419b2
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-15 11:56:28 +0530

    Time: 42 ms (16.85%), Space: 43.7 MB (14.63%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
-    void help(vector<int> temp,vector<vector<int>>& v,vector<int>& candidates,int target,int i){
+    void help(vector<int> temp,vector<vector<int>>& v,vector<int>& candidates,int target,int i,set<vector<int>> &s){
         if(target==0){
+            if(s.find(temp)!=s.end())return;
+            s.insert(temp);
             v.push_back(temp);
             return;
         }
@@ -9,16 +11,17 @@ public:
             return;   
         }
         //not take
-        help(temp,v,candidates,target,i+1);
+        help(temp,v,candidates,target,i+1,s);
         // take
         if(target>=candidates[i]){
             temp.push_back(candidates[i]);
-            help(temp,v,candidates,target-candidates[i],i);
+            help(temp,v,candidates,target-candidates[i],i,s);
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> v;
-        help({},v,candidates,target,0);
+        set<vector<int>> s;
+        help({},v,candidates,target,0,s);
         return v;   
     }
 };
