0ff8e727c70109c5e9ea9a53a26f191507cbbdac
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-15 11:28:28 +0530

    Time: 4 ms (36.33%), Space: 16.2 MB (21.7%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    //n - how many left to open
-    //i - how many are currently open
-    void help(vector<string>& res,string& temp,int n,int i){
-        if(n==0 && i==0){
-            res.push_back(temp);
+    void help(string temp,vector<string>& v,int n,int open){
+        if(n==0 && open==0){
+            v.push_back(temp);
             return;
         }
-
-        if(i>0){
-            temp+=')';
-            help(res,temp,n,i-1);
-            temp.pop_back();
-
-        }
-
-        
-
-
         if(n>0){
+            //'('
             temp+='(';
-            help(res,temp,n-1,i+1);
+            help(temp,v,n-1,open+1);
             temp.pop_back();
         }
-        
-
+        if(open>0){
+            //')'
+            temp+=')';
+            help(temp,v,n,open-1);
+            temp.pop_back();
+        }
     }
-
-
     vector<string> generateParenthesis(int n) {
-        vector<string> res;
-        string temp;
-        help(res,temp,n,0);
-        return res;   
+        vector<string> v;
+        help("",v,n,0);
+        return v;   
     }
 };
