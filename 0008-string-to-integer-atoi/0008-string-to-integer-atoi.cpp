34d6add9ca4bbd90da648f37ce8e88b9ad29c7ee
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-15 10:14:43 +0530

    Time: 0 ms (100%), Space: 10.2 MB (5.31%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
+    long long help(int i,string s,long long ans){
+        if(i>=s.length())return ans;
+        if(s[i]>='0' && s[i]<='9'){
+            int num=s[i]-'0';
+            if(ans < (LONG_MAX-num)/10 ){
+                ans= (ans*10) + (num) ;
+            }
+            else return LONG_MAX;
+            return help(i+1,s,ans);
+        }
+        return ans;
+    }
     int myAtoi(string s) {
         int n=s.length();
         int i=0;
-
-        //ignore leading whitespaces
+        //leading whitespaces
         while(i<n && s[i]==' ')i++;
-        
         //sign
-        int sign=1;
-        if(i<n && (s[i]=='+' || s[i]=='-')){
-            if(s[i]=='-')sign=-1;
+        bool flag=true;
+        if(i<n && s[i]=='-'){
+            flag=false;
             i++;
         }
-
-        //ignore leading zeroes
-        //end at non digit or end of string
-        int ans=0;
-        while(i<n && isdigit(s[i])){
-            int num=s[i]-'0';
-            if( ans > (INT_MAX-num)/10 ){
-                return sign==1?INT_MAX:INT_MIN;
-            }
-            ans=ans*10+num;
+        else if(i<n && s[i]=='+'){
+            flag=true;
             i++;
         }
-
-        return (ans*sign);
+        //number
+        while(i<n && s[i]=='0')i++;
+        long long ans=help(i,s,0);
+        if(!flag)ans=-1*ans;
+        if(ans>INT_MAX)ans=INT_MAX;
+        else if(ans<INT_MIN)ans=INT_MIN;
+        return (int)ans;
     }
 };
