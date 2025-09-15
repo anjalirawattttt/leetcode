719ff135b3f4530bc937b037f98b740136dd92dc
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-15 12:22:42 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.32%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    void helper(vector<string>& res,string &temp,string digits,int i,unordered_map<char,string> m){
-        if(i==digits.length()){
-            res.push_back(temp);
+    void help(string temp,vector<string>& ans,int i,string &digits,unordered_map<int,string> &m){
+        if(i>=digits.length()){
+            if(temp!="")ans.push_back(temp);
             return;
         }
-        
-        for(int k=0;k<m[digits[i]].length();k++){
-            temp+=m[digits[i]][k];
-            helper(res,temp,digits,i+1,m);
+        string mapped=m[ digits[i]-'0' ];
+        for(int j=0;j<mapped.length();j++){
+            temp+=mapped[j];
+            help(temp,ans,i+1,digits,m);
             temp.pop_back();
         }
     }
-    
-    
-
     vector<string> letterCombinations(string digits) {
-        
-        vector<string> res;
-        if(digits.length()==0)return res;
-        string temp;
-        unordered_map<char,string> m={{'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}};
-        helper(res,temp,digits,0,m);
-        return res;    
+        vector<string> ans;
+        unordered_map<int,string> m={
+            {2,"abc"},
+            {3,"def"},
+            {4,"ghi"},
+            {5,"jkl"},
+            {6,"mno"},
+            {7,"pqrs"},
+            {8,"tuv"},
+            {9,"wxyz"}
+        };
+        help("",ans,0,digits,m);
+        return ans;    
     }
 };
