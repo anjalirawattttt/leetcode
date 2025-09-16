60a6523695abebeae00ff1e6567673dd95f9b2d0
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-16 13:05:31 +0530

    Time: 1765 ms (5.01%), Space: 10 MB (5.42%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,51 +1,50 @@
 class Solution {
 public:
-    bool isSafe(vector<vector<char>>& board,int row,int col,char k){
-        for(int i=0;i<9;i++){
-            if(board[row][i]==k)return false;
-            if(board[i][col]==k)return false;
-            // int r = 3*(row/3) + i/3;
-            // int c = 3*(col/3) + i%3;
-            // if(board[r][c]==k)return false;
+    bool isSafe(int r,int c,int x,vector<vector<char>>& board){
+        unordered_set<int> rows;
+        unordered_set<int> cols;
+        unordered_set<int> grid;
+        for(int i=0;i<board.size();i++){
+            if(board[i][c]!='.' && board[i][c]-'0'==x)return false;
         }
-        int ro = 3*(row/3);
-        int co = 3*(col/3);
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-               if(board[i+ro][j+co]==k)return false; 
+
+        for(int j=0;j<board[0].size();j++){
+            if(board[r][j]!='.' && board[r][j]-'0'==x)return false;
+        }
+        int gs,ge;
+        if(r<3)gs=0;
+        else if(r<6)gs=3;
+        else if(r<9)gs=6;
+        if(c<3)ge=0;
+        else if(c<6)ge=3;
+        else if(c<9)ge=6;
+        for(int i=gs;i<gs+3;i++){
+            for(int j=ge;j<ge+3;j++){
+                if(board[i][j]!='.' && board[i][j]-'0'==x)return false;
             }
         }
         return true;
     }
-
-
-
     bool solve(vector<vector<char>>& board){
-
-        for(int row=0;row<9;row++){
-            for(int col=0;col<9;col++){
-
-                if(board[row][col]=='.'){
-                    for(int k='1';k<='9';k++){
-                        if(isSafe(board,row,col,k)){
-                            board[row][col]=k;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                //empty
+                if(board[i][j]=='.'){
+                    for(int k=1;k<=9;k++){
+                        if(isSafe(i,j,k,board)){
+                            board[i][j]=k+'0';
                             if(solve(board))return true;
-                            board[row][col]='.';
+                            board[i][j]='.';
                         }
-
-                    }
-                    //no digit found
+                    }  
                     return false;
                 }
+
             }
         }
-        //all cells filled
         return true;
-        
     }
-
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);   
-
+        solve(board);        
     }
 };
