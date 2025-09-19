a1e4b37d7dd878fe1a527fdf212265d3319eed3a
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-19 14:25:52 +0530

    Time: 75 ms (47.74%), Space: 148.5 MB (65.93%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,51 +1,34 @@
 class MedianFinder {
-    int size;
-    //max heap
-    priority_queue<int> pq;
-    //min heap
-    priority_queue<int,vector<int>,greater<int>> p;
+    //maxheap
+    priority_queue<int> left;
+    //minheap
+    priority_queue<int,vector<int>,greater<int>> right;
 public:
     MedianFinder() {
-        size=0;    
-    }
 
-    void balance(){
-        int n=pq.size();
-        int m=p.size();
-        if(n==m)return;
-        if(n==m+1)return;
-        if(n>m+1){
-            int temp=pq.top();
-            pq.pop();
-            p.push(temp);
-        }
-        else if(m>n){
-            int temp=p.top();
-            p.pop();
-            pq.push(temp);
-        }
     }
     
     void addNum(int num) {
-        if(pq.empty()||num<=pq.top()){
-            pq.push(num);
+        left.push(num);
+        //check bigger smaller data
+        if(!left.empty() && !right.empty() && left.top()>right.top()){
+            right.push(left.top());
+            left.pop();
         }
-        else{
-            p.push(num);
+        //check size of data
+        if(left.size()>right.size()+1){
+            right.push(left.top());
+            left.pop();
         }
-        balance();
-        size++;   
+        else if(right.size()>left.size()){
+            left.push(right.top());
+            right.pop();
+        }
     }
     
     double findMedian() {
-        double ans;
-        if(size%2==1){
-            ans=pq.top();
-        } 
-        else{
-            ans= (double)(pq.top() + p.top())/2.0;
-        }  
-        return ans; 
+        if(left.size()==right.size())return (left.top()+right.top())/2.0;   
+        return left.top();    
     }
 };
 
