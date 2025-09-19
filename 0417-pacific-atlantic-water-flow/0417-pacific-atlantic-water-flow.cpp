06727a2c5564157f10f926e8aa01f69139cda194
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-19 12:13:27 +0530

    Time: 23 ms (20.03%), Space: 26.5 MB (22.28%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<int>>& heights,vector<vector<bool>> &o){
-        o[i][j]=true;
-        vector<int> rows={0,0,1,-1};
-        vector<int> cols={1,-1,0,0};
+    void help(int i,int j,vector<vector<bool>> &visited,vector<vector<int>>& heights){
+        visited[i][j]=true;
+        vector<int> rows={1,-1,0,0};
+        vector<int> cols={0,0,1,-1};
         for(int k=0;k<4;k++){
             int r=i+rows[k];
             int c=j+cols[k];
-            if(r>=0 && c>=0 && r<heights.size() && c<heights[0].size() && heights[r][c]>=heights[i][j] && !o[r][c]){
-                dfs(r,c,heights,o);
+            if(r>=0 && c>=0 && r<heights.size() && c<heights[0].size() && !visited[r][c] && heights[r][c]>=heights[i][j]){
+                help(r,c,visited,heights);
             }
         }
     }
-
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        vector<vector<int>> ans;
         int m=heights.size();
         int n=heights[0].size();
-        vector<vector<bool>> p(m,vector<bool>(n,false));
-        vector<vector<bool>> a(m,vector<bool>(n,false));
-        
-        //first row
-        for(int j=0;j<n;j++)dfs(0,j,heights,p);
-
-        //first col
-        for(int i=0;i<m;i++)dfs(i,0,heights,p);
-
-        
-
-
-        //last row
-        for(int j=0;j<n;j++)dfs(m-1,j,heights,a);
-
-        //last col
-        for(int i=0;i<m;i++)dfs(i,n-1,heights,a);
-
+        vector<vector<bool>> vp(m,vector<bool>(n,false));
+        vector<vector<bool>> va(m,vector<bool>(n,false));
+        vector<vector<int>> ans;
+        for(int i=0;i<m;i++){
+            if(!vp[i][0])help(i,0,vp,heights);
+            if(!va[i][n-1])help(i,n-1,va,heights);
+        }
+        for(int j=0;j<n;j++){
+            if(!vp[0][j])help(0,j,vp,heights);
+            if(!va[m-1][j])help(m-1,j,va,heights);
+        }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(p[i][j] && a[i][j]){
-                    ans.push_back({i,j});
-                }
+                if(vp[i][j] && va[i][j])ans.push_back({i,j});
             }
         }
-        return ans;
-
+        return ans;          
     }
 };
