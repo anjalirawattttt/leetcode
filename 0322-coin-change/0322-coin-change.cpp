4ad761f641fe2ae1954be93601a3d4b7dbd397ea
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-20 22:57:04 +0530

    Time: 51 ms (25.3%), Space: 49.8 MB (22.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    // int help(vector<int>& coins, int amount,int i,vector<vector<int>> &dp){
-    //     if(amount==0)return 0;
-    //     if(i<0)return INT_MAX;
-    //     if(dp[i][amount]!=-1)return dp[i][amount];
-    //     //pick
-    //     int op1=1e7;
-    //     if(amount>=coins[i])op1=1+help(coins,amount-coins[i],i,dp);
-    //     //not pick
-    //     int op2=help(coins,amount,i-1,dp);
-    //     return dp[i][amount]=min(op1,op2);
-
-    // }
+    int help(int i,vector<int>& coins,int amount,vector<vector<int>> &dp){
+        if(amount==0)return 0;
+        if(i<0)return 1e7;
+        if(dp[i][amount]!=-1)return dp[i][amount];
+        //take
+        int op1=1e7;
+        if(amount>=coins[i])op1=1+help(i,coins,amount-coins[i],dp);
+        //not take
+        int op2=help(i-1,coins,amount,dp);
+        return dp[i][amount]=min(op1,op2);
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,1e7));
-        for(int i=0;i<n;i++)dp[i][0]=0;
-
-        for(int j=1;j<=amount;j++){
-            if(j%coins[0]==0)dp[0][j]=j/coins[0];
-        }
-
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=amount;j++){
-                //pick
-                int op1=1e7;
-                if(j>=coins[i])op1=1+dp[i][j-coins[i]];
-                //not pick
-                int op2=dp[i-1][j];
-                dp[i][j]=min(op1,op2);
-            }
-        }
-        int x=dp[n-1][amount];
-        return x>=1e7?-1:x;    
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int ans=help(n-1,coins,amount,dp);
+        return ans>=1e7?-1:ans;    
     }
 };
