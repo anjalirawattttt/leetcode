9b9480b05dbce4a128a8519091b1649e88cf6210
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-20 12:21:48 +0530

    Time: 3 ms (89.46%), Space: 23.8 MB (57.64%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    static bool myCmp(vector<int> &a,vector<int> &b){
-        return a[0]<b[0];
-    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
-        sort(intervals.begin(),intervals.end(),myCmp);   
-
-        int s=intervals[0][0],e=intervals[0][1];
-        for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]>=s && intervals[i][0]<=e){
-                s=min(s,intervals[i][0]);
-                e=max(e,intervals[i][1]);
+        sort(intervals.begin(),intervals.end()); 
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        int i=1,n=intervals.size();
+        while(i<n){
+            if(intervals[i][0]<=end){
+                start=min(start,intervals[i][0]);
+                end=max(end,intervals[i][1]);
             }
             else{
-                ans.push_back({s,e});
-                s=intervals[i][0];
-                e=intervals[i][1];
+                ans.push_back({start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
             }
+            i++;
         }
-        ans.push_back({s,e});
-        return ans; 
+        ans.push_back({start,end});
+        return ans;
     }
 };
