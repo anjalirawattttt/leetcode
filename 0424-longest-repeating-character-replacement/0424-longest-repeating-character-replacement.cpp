bceb0b485fc560fcf7f8c353c14d3df717e018f3
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-20 08:50:41 +0530

    Time: 15 ms (16.6%), Space: 10.9 MB (40.05%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int maxLen=0;
-        unordered_map<char,int> m;
+        int ans=0;
         int n=s.length();
-        int l=0,r=0,maxFreq=0; 
-
+        int l=0,r=0;
+        unordered_map<char,int> m;
+        int maxFreq=0;
         while(r<n){
-            m[s[r]]++;  
-            if(m[s[r]]>maxFreq)maxFreq=m[s[r]];
-            if((r-l+1)-maxFreq>k){
+            m[s[r]]++;
+            maxFreq=max(maxFreq,m[s[r]]);
+            if(r-l+1-maxFreq<=k)ans=max(ans,r-l+1);
+            else{
                 m[s[l]]--;
                 l++;
+                maxFreq=0;
+                for(auto &p:m){
+                    maxFreq=max(maxFreq,p.second);
+                }
             }
-            if((r-l+1)-maxFreq<=k)maxLen=max(maxLen,r-l+1); 
             r++;
         }
-        return maxLen;  
+        return ans;    
     }
 };
