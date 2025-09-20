c4ff79415603aa983a3b12b6596e2b0269d23b66
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-20 23:02:00 +0530

    Time: 54 ms (19.89%), Space: 53 MB (14.89%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -13,8 +13,23 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans=help(n-1,coins,amount,dp);
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
+        //base case i=0
+        for(int a=0;a<=amount;a++)dp[0][a]=1e7;
+        //base case amount ==0 =>0
+        //i-1
+        for(int i=1;i<=n;i++){
+            for(int a=1;a<=amount;a++){
+                //take
+                int op1=1e7;
+                if(a>=coins[i-1])op1=1+dp[i][a-coins[i-1]];
+                //not take
+                int op2=dp[i-1][a];
+                dp[i][a]=min(op1,op2);    
+            }
+        }
+
+        int ans=dp[n][amount];
         return ans>=1e7?-1:ans;    
     }
 };
