dff45572c1a80006fc5167d03d3665b5dc537051
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-20 23:04:09 +0530

    Time: 47 ms (36.17%), Space: 87.7 MB (5.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    int help(int i,vector<int>& coins,int amount,vector<vector<int>> &dp){
-        if(amount==0)return 0;
-        if(i<0)return 1e7;
-        if(dp[i][amount]!=-1)return dp[i][amount];
-        //take
-        int op1=1e7;
-        if(amount>=coins[i])op1=1+help(i,coins,amount-coins[i],dp);
-        //not take
-        int op2=help(i-1,coins,amount,dp);
-        return dp[i][amount]=min(op1,op2);
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
-        //base case i=0
-        for(int a=0;a<=amount;a++)dp[0][a]=1e7;
+        vector<int> prev(amount+1,1e7);
         //base case amount ==0 =>0
+        prev[0]=0;
         //i-1
         for(int i=1;i<=n;i++){
+            vector<int> curr(amount+1,1e7);
+            curr[0]=0;
             for(int a=1;a<=amount;a++){
                 //take
                 int op1=1e7;
-                if(a>=coins[i-1])op1=1+dp[i][a-coins[i-1]];
+                if(a>=coins[i-1])op1=1+curr[a-coins[i-1]];
                 //not take
-                int op2=dp[i-1][a];
-                dp[i][a]=min(op1,op2);    
+                int op2=prev[a];
+                curr[a]=min(op1,op2);    
             }
+            prev=curr;
         }
 
-        int ans=dp[n][amount];
+        int ans=prev[amount];
         return ans>=1e7?-1:ans;    
     }
 };
