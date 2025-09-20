8b148fbfdf094d18c04e3d473543a52a68d4447b
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-20 22:49:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -11,7 +11,17 @@ public:
         return dp[n]=op1+op2;
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return help(n,dp);   
+        vector<int> dp(n+2,0);
+        //n==1 -> 1
+        dp[1]=1;
+        for(int i=2;i<=n+1;i++){
+            //1
+            int op1=dp[i-1];
+            //2
+            int op2=dp[i-2];
+            dp[i]=op1+op2;
+        }
+        
+        return dp[n+1];   
     }
 };
