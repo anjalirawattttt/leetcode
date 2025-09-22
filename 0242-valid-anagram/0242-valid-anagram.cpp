ffe85c066393097784dff4fcb1164358d558b381
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-22 08:44:42 +0530

    Time: 0 ms (100%), Space: 9.7 MB (48.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length())return false;
-
-        int n=s.length();
         vector<int> hash(26,0);
-        for(int i=0;i<n;i++){
-            hash[s[i]-'a']++;
-            hash[t[i]-'a']--;
+        for(char &ch : s){
+            hash[ch-'a']++;
         }  
-        for(int i=0;i<26;i++){
-            if(hash[i]!=0)return false;
+        for(char &ch : t){
+            hash[ch-'a']--;
+            if(hash[ch-'a']<0)return false;
         }
-        return true; 
+        return true;
     }
 };
