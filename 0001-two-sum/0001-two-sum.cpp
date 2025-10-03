11e1a9b58cbc59c83e9a11d0a0a96df738a4d65b
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-10-03 13:25:00 +0530

    Time: 3 ms (69.58%), Space: 14.8 MB (50.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans(2,-1);
-        int n=nums.size();
         vector<pair<int,int>> v;
+        int n=nums.size();
         for(int i=0;i<n;i++){
             v.push_back({nums[i],i});
-        }
+        } 
         sort(v.begin(),v.end());
         int l=0,r=n-1;
         while(l<r){
             int sum=v[l].first+v[r].first;
             if(sum==target){
-                ans[0]=v[l].second;
-                ans[1]=v[r].second;
-                break;
+                return {v[l].second,v[r].second};
             }
-            else if(sum<target)l++;
-            else r--;
-        }
-        sort(ans.begin(),ans.end());
-        return ans;    
+            else if(sum<target){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }  
+        return {-1,-1}; 
     }
 };
