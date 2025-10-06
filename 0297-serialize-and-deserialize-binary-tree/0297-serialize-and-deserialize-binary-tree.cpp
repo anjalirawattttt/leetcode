daaadeb053f2d0829c03d5dabb85b00f3b2995cd
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-10-06 19:23:45 +0530

    Time: 38 ms (16.3%), Space: 39.8 MB (24.09%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -9,45 +9,54 @@
  */
 class Codec {
 public:
-    void serializeHelp(string& ans,TreeNode* root){
+    
+    void shelp(TreeNode* root,string &tree){
         if(!root){
-            ans+='n';
-            ans+='s';
+            tree+="null,";
             return;
         }
-        ans+=to_string(root->val);
-        ans+='s';
-        serializeHelp(ans,root->left);
-        serializeHelp(ans,root->right);
+        tree+=to_string(root->val) + ",";
+        shelp(root->left,tree);
+        shelp(root->right,tree);    
     }
-
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        string ans="";
-        serializeHelp(ans,root);
-        return ans;
+        string tree="";
+        shelp(root,tree);
+        return tree;     
     }
 
-    TreeNode* deserializeHelp(int &i,string data){
-        string temp="";
-        while(data[i]!='s'){
-            temp+=data[i];
+
+    TreeNode* buildTree(vector<string> &nodeVals,int& i){
+        if(i>=nodeVals.size() || nodeVals[i]=="null"){
             i++;
+            return NULL;
         }
-        i++;
-        if(temp=="n")return NULL;
-        TreeNode* node=new TreeNode(stoi(temp));
-        node->left=deserializeHelp(i,data);
-        node->right=deserializeHelp(i,data);
-        return  node;        
+        TreeNode* node=new TreeNode(stoi(nodeVals[i++]));
+        node->left=buildTree(nodeVals,i);
+        node->right=buildTree(nodeVals,i);
+        return node;
     }
-
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        TreeNode* root;
+        vector<string> nodeVals;
+        string token="";
+
+        for(char ch:data){
+            if(ch==','){
+                nodeVals.push_back(token);
+                token.clear();
+            }
+            else{
+                token+=ch;
+            }
+        }  
+        //nothing after comma
         int i=0;
-        root=deserializeHelp(i,data);
-        return root;    
+        return buildTree(nodeVals,i);
+
+        
+
     }
 };
 
