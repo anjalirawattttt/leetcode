5a3dacc1ff8cc229ddf2701a1222e8df3a2451aa
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-10-08 22:20:35 +0530

    Time: 23 ms (60.8%), Space: 24 MB (38.3%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> ans;
+        if(words.empty() || s.empty())return ans;
+
         unordered_map<string,int> m;
         for(string &word:words){
             m[word]++;
@@ -10,30 +12,29 @@ public:
         int wordLen=words[0].length();
         int totalLen=wordLen * words.size();
         int n=s.length();
+        if(n<totalLen)return ans;
 
-        for(int i=0;i<=n-totalLen;i++){
-            int l=i,r=i+totalLen; 
-            int count=0;
-            unordered_map<string,int> seen; 
-            while(l<r){
-                string temp=s.substr(l,wordLen);
-                if(m.find(temp)==m.end()){
-                    break;
-                }
-                else{
+        for(int i=0;i<wordLen;i++){
+            int left=i,count=0;
+            unordered_map<string,int> seen;
+            for(int j=i;j+wordLen<=n;j+=wordLen){
+                string temp=s.substr(j,wordLen);
+                if(m.find(temp)!=m.end()){
                     seen[temp]++;
                     count++;
-                }
-                l+=wordLen;
-            }  
-            if(count==words.size()){
-                for(auto &p:m){
-                    if(m[p.first]!=seen[p.first]){
-                        count=-1;
-                        break;
+                    while(seen[temp]>m[temp]){
+                        string leftWord=s.substr(left,wordLen);
+                        seen[leftWord]--;
+                        count--;
+                        left+=wordLen;
                     }
+                    if(count==words.size())ans.push_back(left);
                 }
-                if(count!=-1)ans.push_back(i);
+                else{
+                    count=0;
+                    seen.clear();
+                    left=j+wordLen;
+                }
             }
 
         }
