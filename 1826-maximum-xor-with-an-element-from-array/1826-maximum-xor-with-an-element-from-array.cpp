2cfd1222bfedc60e568218f99dc9d58e8956dd85
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-10-27 16:34:37 +0530

    Time: 479 ms (23.52%), Space: 235.1 MB (31.45%) - LeetHub

---
+++ 1826-maximum-xor-with-an-element-from-array/1826-maximum-xor-with-an-element-from-array.cpp
@@ -0,0 +1,81 @@
+struct Node{
+    Node* links[2];
+    bool containsBit(int bit){
+        return links[bit]!=NULL;
+    }
+    void put(int bit,Node* node){
+        links[bit]=node;
+    }
+    Node* get(int bit){
+        return links[bit];
+    }
+};
+class Trie{
+    private:
+    Node* root;
+    public:
+    Trie(){
+        root=new Node();
+    }
+    void insert(int num){
+        Node* node= root;
+        for(int i=31;i>=0;i--){
+            int bit=(num>>i)&1;
+            if(!node->containsBit(bit)){
+                node->put(bit,new Node());
+            }
+            node=node->get(bit);
+        }
+    }
+    int getMax(int num){
+        int ans=0;
+        Node* node= root;
+        for(int i=31;i>=0;i--){
+            int bit=(num>>i)&1;
+            if(node->containsBit(1-bit)){
+                ans=ans|(1<<i);
+                node=node->get(1-bit);
+            }
+            else{
+                node=node->get(bit);
+            }
+        }
+        return ans;
+    }
+};
+class Solution {
+public:
+    static bool myCmp(const vector<int> &a,const vector<int> &b){
+        return a[1]<b[1];
+    }
+    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=queries.size();
+        int size=nums.size();
+        vector<int> ans(n);
+
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> temp(queries.begin(),queries.end());
+        for(int i=0;i<n;i++){
+            temp[i].push_back(i);
+        }  
+        sort(temp.begin(),temp.end(),myCmp);  
+
+        Trie* t=new Trie();
+        int idx=0;
+        for(int i=0;i<n;i++){
+            int maxXOR=-1;
+            while(idx<size && nums[idx]<=temp[i][1]){
+                t->insert(nums[idx]);
+                idx++;
+            }
+            if(idx==0){
+                ans[temp[i][2]]=maxXOR;
+                continue;
+            }
+            maxXOR=max(maxXOR,t->getMax(temp[i][0]));
+            ans[temp[i][2]]=maxXOR;
+
+        }
+        return ans;
+    }
+};
