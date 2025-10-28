91942e2172fcb08df8e79eb59ac94b17c380986a
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-10-28 15:36:21 +0530

    Time: 11 ms (14.98%), Space: 15.8 MB (38.2%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
+        //using KMP
         int n=s.length();
+
         string rev=s;
         reverse(rev.begin(),rev.end());
 
-        for(int i=0;i<=n;i++){
-            if(!memcmp(s.c_str(),rev.c_str()+i,n-i)){
-                int rem=i;
-                return rev+s.substr(n-i,i);
+        string temp=s+"-"+rev;
+        int size=2*n+1;
+
+        vector<int> lps(size,0);
+        int len=0;
+        int i=1;
+        while(i<size){
+            if(temp[i]==temp[len]){
+                len++;
+                lps[i]=len;
+                i++;
             }
-        } 
-        return rev+s;
+            else if(len>0){
+                len=lps[len-1];
+            }
+            else{
+                lps[i]=0;
+                i++;
+            }
+        }
+        int pal=lps[size-1]; 
+        int rem=n-pal;
+        return rev.substr(0,rem)+s;
     }
 };
