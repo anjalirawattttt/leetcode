12bf28fdc46aa8c9a04e50bbff9f9c0a84ade847
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-03 23:21:13 +0530

    Time: 67 ms (19.35%), Space: 30.9 MB (6.09%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+    int gcd(int a,int b){
+        if(b==0)return a;
+        return gcd(b,a%b);
+    }
     int maxPoints(vector<vector<int>>& points) {
         int n=points.size();
         int result=1;
         for(int i=0;i<n;i++){
-            unordered_map<double,int> m;
+            unordered_map<string,int> m;
             int maxCount=1;
             for(int j=0;j<n;j++){
                 if(i==j)continue;
                 int dy=points[j][1]-points[i][1];
                 int dx=points[j][0]-points[i][0];
-                double theta=atan2(dy,dx);
-                m[theta]++;
-                if(m[theta]+1>maxCount){
-                    maxCount=m[theta]+1;
+                int g=gcd(dy,dx);
+                dy/=g;
+                dx/=g;
+                string temp=to_string(dy)+"_"+to_string(dx);
+                m[temp]++;
+                if(m[temp]+1>maxCount){
+                    maxCount=m[temp]+1;
                 }
             }
             result=max(result,maxCount);
