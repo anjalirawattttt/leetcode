7112bf6f3e41a241e8c89515a6c0d2d96059df8b
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-04 19:08:18 +0530

    Time: 43 ms (72.12%), Space: 13.1 MB (69.76%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int help(int num,vector<int> &dp){
-        if(num==0)return 0;
-        if(dp[num]!=-1)return dp[num];
+    // int help(int num,vector<int> &dp){
+    //     if(num==0)return 0;
+    //     if(dp[num]!=-1)return dp[num];
         
 
-        int op=INT_MAX;
-        for(int i=num;i>=1;i--){
-            int sq=(int) sqrt(i);
-            if(sq*sq==i){
-                op=min(op,num/i+help(num%i,dp));
-            }
-        }
-        return dp[num]=op;
-    }
+    //     int op=INT_MAX;
+    //     for(int i=num;i>=1;i--){
+    //         int sq=(int) sqrt(i);
+    //         if(sq*sq==i){
+    //             op=min(op,num/i+help(num%i,dp));
+    //         }
+    //     }
+    //     return dp[num]=op;
+    // }
     int numSquares(int n) {
-        vector<int> dp(n+1,-1);
-        return help(n,dp);    
+        vector<int> dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int num=1;num<=n;num++){
+            for(int i=1;i*i<=num;i++){
+                int square=i*i;
+                dp[num]=min(dp[num],1+dp[num-square]);
+            }  
+        }
+
+
+
+        return dp[n];    
     }
 };
