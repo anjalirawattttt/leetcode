6b893a16834e3ecff984671b6b6a75555755ba69
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-05 14:45:50 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.5%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int help(int i,vector<int>& nums,vector<int> &dp){
+    int help(vector<int>& nums,int i,vector<int> &dp){
+        if(i==0)return nums[0];
         if(i<0)return 0;
         if(dp[i]!=-1)return dp[i];
-        //take
-        int op1=nums[i]+help(i-2,nums,dp);
-        //not take
-        int op2=help(i-1,nums,dp);
+        int op1=nums[i]+help(nums,i-2,dp);
+        int op2=help(nums,i-1,dp);
         return dp[i]=max(op1,op2);
     }
+
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+2,0);
-        for(int i=2;i<=n+1;i++){
-            //take
-            int op1=nums[i-2]+dp[i-2];
-            //not take
-            int op2=dp[i-1];
-            dp[i]=max(op1,op2);
-        }
-        return dp[n+1];    
+        vector<int> dp(n,-1);
+        return help(nums,n-1,dp);    
     }
 };
