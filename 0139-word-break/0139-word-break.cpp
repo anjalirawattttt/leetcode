463ba7a8167f0e9d1737e698c5a9bfad60d7432e
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-05 16:08:58 +0530

    Time: 1 ms (76.23%), Space: 12.1 MB (67.44%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    bool help(int i,string& s,unordered_set<string>& st,vector<int>& dp){
-        if(i==s.length())return true;
-        if(dp[i]!=-1)return dp[i];
+    bool help(int idx,string &s,unordered_set<string> &st,vector<int> &dp){
+        if(idx>=s.length())return true;
+        if(dp[idx]!=-1)return dp[idx];
         string temp="";
-        for(int j=i;j<s.length();j++){
-            temp+=s[j];
+        for(int i=idx;i<s.length();i++){
+            temp+=s[i];
             if(st.find(temp)!=st.end()){
-                if(help(j+1,s,st,dp))return true;
+                if(help(i+1,s,st,dp))return true;
             }
         }
-        return dp[i]=false;
+        return dp[idx]=false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> st(wordDict.begin(),wordDict.end());
-        int n=s.length();
-        vector<int> dp(n,-1);
-        if( help(0,s,st,dp) )return true;
-        return false;
+        unordered_set<string> st;
+        for(string &word:wordDict){
+            st.insert(word);
+        } 
+        vector<int> dp(s.length(),-1);
+        return help(0,s,st,dp);   
     }
 };
