33a055b27563f595772a987123343d4b2c41288f
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-05 18:54:01 +0530

    Time: 1 ms (82.37%), Space: 18.3 MB (59.13%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> topSort;
-        vector<int> inDegrees(numCourses,0);
+        vector<int> ans;
         vector<vector<int>> adj(numCourses);
-
-        for(auto &edge:prerequisites){
-            inDegrees[edge[0]]++;
-            adj[edge[1]].push_back(edge[0]);
-        }
-
+        vector<int> indegrees(numCourses,0);
+        for(auto &p:prerequisites){
+            adj[p[1]].push_back(p[0]);
+            indegrees[p[0]]++;
+        } 
         queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(inDegrees[i]==0)q.push(i);
+            if(indegrees[i]==0)q.push(i);
         }
-
         while(!q.empty()){
             int u=q.front();
             q.pop();
-            topSort.push_back(u);
+            ans.push_back(u);
             for(int &v:adj[u]){
-                inDegrees[v]--;
-                if(inDegrees[v]==0)q.push(v);
+                indegrees[v]--;
+                if(indegrees[v]==0)q.push(v);
             }
         }
-        if(topSort.size()!=numCourses )return {};
-        return topSort;   
+
+        for(int i=0;i<numCourses;i++){
+            if(indegrees[i]!=0)return {};
+        }
+
+        return ans;    
     }
 };
