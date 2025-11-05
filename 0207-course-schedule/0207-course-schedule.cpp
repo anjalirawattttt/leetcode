b7d5cfeb7d542ec99c8a7aaee2f63564c29190aa
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-05 18:52:38 +0530

    Time: 4 ms (67.29%), Space: 19.3 MB (73.52%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,33 +2,32 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        //build adj
-        for(int i=0;i<prerequisites.size();i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        vector<int> indegrees(numCourses,0);
+        for(auto &p:prerequisites){
+            adj[p[1]].push_back(p[0]);
+            indegrees[p[0]]++;
         } 
-        //build inDegrees
-        vector<int> inDegrees(numCourses,0);
-        for(int i=0;i<numCourses;i++){
-            for(int& v:adj[i])  {
-                inDegrees[v]++;
-            } 
-        }  
         queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(inDegrees[i]==0)q.push(i);
+            if(indegrees[i]==0)q.push(i);
         }
         while(!q.empty()){
             int u=q.front();
             q.pop();
             for(int &v:adj[u]){
-                inDegrees[v]--;
-                if(inDegrees[v]==0)q.push(v);
+                indegrees[v]--;
+                if(indegrees[v]==0)q.push(v);
             }
         }
+
         for(int i=0;i<numCourses;i++){
-            if(inDegrees[i]!=0)return false;
+            if(indegrees[i]!=0)return false;
         }
+
         return true;
+
+
+
 
     }
 };
