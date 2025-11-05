9225a725b483af1135154f10f324cbd10de25308
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-05 16:14:48 +0530

    Time: 0 ms (100%), Space: 9.5 MB (18.1%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-    // int help(int i,int j,vector<vector<int>> &dp){
-    //     if(i==0 && j==0)return 1;
-    //     if(i<0 || j<0)return 0;
-    //     if(dp[i][j]!=-1)return dp[i][j];
-    //     //top
-    //     int op1=help(i-1,j,dp);
-    //     //left
-    //     int op2=help(i,j-1,dp);
-    //     return dp[i][j]=op1+op2;
-    // }
+    int help(int i,int j,vector<vector<int>> &dp){
+        if(i==0 && j==0)return 1;
+        if(i<0 || j<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        //up
+        int op1=help(i-1,j,dp);
+        //left
+        int op2=help(i,j-1,dp);
+        return dp[i][j] = op1+op2;
+    }
     int uniquePaths(int m, int n) {
-        vector<int> prev(n+1,0);
-        //base case i->0 || j->0 ==> dp->0
-
-        for(int i=1;i<=m;i++){
-            vector<int> curr(n+1,0);
-            if(i==1)curr[1]=1;
-            for(int j=1;j<=n;j++){
-                if(i==1 && j==1)continue;
-                //top
-                int op1=prev[j];
-                //left
-                int op2=curr[j-1];
-                curr[j]=op1+op2;
-            }
-            prev=curr;
-        }
-
-
-        return prev[n];    
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return help(m-1,n-1,dp);    
     }
 };
