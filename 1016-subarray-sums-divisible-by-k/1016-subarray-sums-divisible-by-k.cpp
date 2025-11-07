cfcc9a3d27e10d7895d831dda4665128806472bb
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-07 22:39:33 +0530

    Time: 12 ms (46.36%), Space: 35.5 MB (87.53%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -4,18 +4,14 @@ public:
         int n=nums.size();
         int sum=0;
         unordered_map<int,int> m;
-        // m[0]=1;
+        m[0]=1;
         int ans=0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
-            if(sum%k==0)ans++;
-            for(auto &p:m){
-                int num=p.first;
-                int freq=p.second;
-                if((sum-num)%k==0)ans+=freq;
-            }
-            
-            m[(sum)%k]++;   
+            int mod=sum%k;
+            if(mod<0)mod+=k;
+            ans+=m[mod]; 
+            m[mod]++;  
         }
         return ans;
 
