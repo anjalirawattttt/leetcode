6eddacd414c0bb83ecb1635cb541df049447dd8d
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-07 21:18:24 +0530

    Time: 0 ms (100%), Space: 18.7 MB (51.7%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,8 +3,8 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        vector<int> rows (m,0);  
-        vector<int> cols (n,0);  
+        vector<int> rows(m,0);
+        vector<int> cols(n,0);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
@@ -12,13 +12,13 @@ public:
                     cols[j]=1;
                 }
             }
-        } 
+        }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(rows[i] || cols[j]){
+                if(cols[j] || rows[i]){
                     matrix[i][j]=0;
                 }
             }
-        } 
+        }    
     }
 };
