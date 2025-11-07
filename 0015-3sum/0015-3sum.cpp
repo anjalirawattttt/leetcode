baedb59592f292491b0b1e0312b81b4f37019693
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-07 23:54:12 +0530

    Time: 47 ms (69.81%), Space: 29 MB (72.56%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        //convert it to 2 sum problem
-        //num1+num2+num3=0
-        //num2+num3=-num1
-        int i=0,j,k;
-        while(i<n-2){
-            int sum=-1*nums[i];
-            j=i+1;
-            k=n-1;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int j=i+1;
+            int k=n-1;
             while(j<k){
-                if(nums[j]+nums[k]==sum){
+                int sum=nums[i]+nums[j];
+                if(sum==-nums[k]){
                     ans.push_back({nums[i],nums[j],nums[k]});
                     while(j<n-1 && nums[j]==nums[j+1])j++;
-                    while(k>0 && nums[k]==nums[k-1])k--;
-                    k--;
+                    j++;        
                 }
-                else if(nums[j]+nums[k]<sum){
+                else if(sum<-nums[k]){
                     j++;
                 }
                 else{
                     k--;
                 }
             }
-            while(i<n-1 && nums[i]==nums[i+1])i++;
-            i++;
         }
-        return ans;
+        return ans;    
     }
 };
