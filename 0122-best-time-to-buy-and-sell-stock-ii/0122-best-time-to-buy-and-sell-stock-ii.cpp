1836d86c98e08d042668d81d46950b9564d10244
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-07 21:34:09 +0530

    Time: 0 ms (100%), Space: 17.2 MB (42.77%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,44 +1,16 @@
 class Solution {
 public:
-    // int help(int i,int buy,vector<int>& prices,vector<vector<int>>& dp){
-    //     if(i==prices.size())return 0;
-    //     long profit=0;
-    //     if(dp[buy][i]!=-1)return dp[buy][i];
-    //     if(buy){
-    //         //buy-not buy
-    //         profit=max(-prices[i]+help(i+1,0,prices,dp),help(i+1,1,prices,dp));
-    //     }
-    //     else{
-    //         //sell-not sell
-    //         profit=max(+prices[i]+help(i+1,1,prices,dp),help(i+1,0,prices,dp));
-    //     }
-    //     return dp[buy][i]=profit;
-    // }
-
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<int> next(2,0);
-        // dp[n][0]=0;
-        // dp[n][1]=0;
-
-        for(int i=n-1;i>=0;i--){
-            vector<int> curr(2,0);
-            for(int buy=0;buy<=1;buy++){
-                
-                if(buy){
-                    //buy-not buy
-                    curr[buy]=max(-prices[i]+next[0],next[1]);
-                }
-                else{
-                    //sell-not sell
-                    curr[buy]=max(+prices[i]+next[1],next[0]);
-                }
+        int ans=0;
+        int minPrice=prices[0];
+        for(int i=0;i<n;i++){
+            if(prices[i]>minPrice){
+                ans+=prices[i]-minPrice;
+                minPrice=prices[i];
             }
-            next=curr;
+            minPrice=min(minPrice,prices[i]);
         }
-
-
-
-        return next[1];    
+        return ans;    
     }
 };
