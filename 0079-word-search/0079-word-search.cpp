cb18f8c066a9da092cb1f2e9401d2461bfce4ef9
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-08 21:08:20 +0530

    Time: 255 ms (60.26%), Space: 10.5 MB (72.27%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> rows={1,-1,0,0};
-    vector<int> cols={0,0,1,-1};
-    bool bfs(int i,int j,vector<vector<char>>& board,int idx,string word){
+    vector<int> cols={0,0,1,-1};    
+    bool bfs(int i,int j,int idx,vector<vector<char>> &board,string &word){
         if(idx==word.length()-1)return true;
         char ch=board[i][j];
         board[i][j]='#';
         for(int k=0;k<4;k++){
             int r=i+rows[k];
             int c=j+cols[k];
-            if(r>=0 && r<board.size() && c>=0 && c<board[0].size() && board[r][c]!='#' && board[r][c]==word[idx+1]){
-                if(bfs(r,c,board,idx+1,word)){
+            if(r>=0 && r<board.size() && c>=0 && c<board[0].size() && board[r][c]==word[idx+1] && board[r][c]!='#'){
+                if(bfs(r,c,idx+1,board,word)){
                     board[i][j]=ch;
                     return true;
                 }
@@ -19,16 +19,17 @@ public:
         board[i][j]=ch;
         return false;
     }
+
     bool exist(vector<vector<char>>& board, string word) {
         int m=board.size();
         int n=board[0].size();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]==word[0]){
-                    if(bfs(i,j,board,0,word))return true;
+                    if(bfs(i,j,0,board,word))return true;
                 }
             }
-        } 
-        return false;   
+        }  
+        return false;  
     }
 };
