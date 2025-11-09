adf551685476e1d25374b906b5a22140e5b141bb
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-09 10:22:07 +0530

    Time: 373 ms (70.32%), Space: 147.1 MB (74.88%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,56 +1,58 @@
 class Solution {
 public:
-    void merge(vector<int> &arr,int l,int m,int r,int &ans){
-        int n1=m-l+1;
-        int n2=r-m;
+    void merge(int l,int mid,int h,vector<int> &nums,int &count){
+        int n1=mid-l+1;
+        int n2=h-mid;
         vector<int> left(n1);
         vector<int> right(n2);
-        for(int i=0;i<n1;i++)left[i]=arr[l+i];
-        for(int i=0;i<n2;i++)right[i]=arr[m+1+i];
+        for(int i=0;i<n1;i++){
+            left[i]=nums[l+i];
+        }
+        for(int i=0;i<n2;i++){
+            right[i]=nums[mid+1+i];
+        }
 
+        //count pairs
         int i=0,j=0;
-        while(i<n1){
-            while(j<n2 && left[i]>(long long)2*right[j]){
+        while(j<n2 && i<n1){
+            if((long long)left[i]>(2LL*right[j])){
+                count+=n1-i;
                 j++;
             }
-            ans+=j;
-            i++;
+            else{
+                i++;
+            }
         }
 
-
-
-
-
-        i=0;j=0;
         int k=l;
+        i=0,j=0;
         while(i<n1 && j<n2){
             if(left[i]<=right[j]){
-                arr[k++]=left[i++];
+                nums[k++]=left[i++];
             }
             else{
-                arr[k++]=right[j++];
+                nums[k++]=right[j++];
             }
         }
         while(i<n1){
-            arr[k++]=left[i++];
+            nums[k++]=left[i++];
         }
         while(j<n2){
-            arr[k++]=right[j++];
+            nums[k++]=right[j++];
         }
     }
-
-    void mergeSort(vector<int> &arr,int l,int r,int &ans){
-        if(l<r){
-            int m=l+(r-l)/2;
-            mergeSort(arr,l,m,ans);
-            mergeSort(arr,m+1,r,ans);
-            merge(arr,l,m,r,ans);
+    void mergeSort(int l,int h,vector<int> &nums,int &count){
+        if(l<h){
+            int mid = l + (h-l)/2 ;
+            mergeSort(l,mid,nums,count);
+            mergeSort(mid+1,h,nums,count);
+            merge(l,mid,h,nums,count);
         }
     }
     int reversePairs(vector<int>& nums) {
+        int count=0;
         int n=nums.size();
-        int ans=0;
-        mergeSort(nums,0,n-1,ans);
-        return ans;    
+        mergeSort(0,n-1,nums,count);
+        return count;    
     }
 };
