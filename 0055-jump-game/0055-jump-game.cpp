da5807b125904788bfac3c26462bc192dac2e030
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-11 13:43:47 +0530

    Time: 1814 ms (5.01%), Space: 54.8 MB (9.36%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    bool help(int idx,vector<int> &nums,vector<int> &dp){
-        if(idx==nums.size()-1)return true;
-        if(dp[idx]!=-1)return dp[idx];
-        for(int i=1;i<=nums[idx];i++){
-            if(help(idx+i,nums,dp))return true;
-        }
-        return dp[idx]=false;
-    }
+    // bool help(int idx,vector<int> &nums,vector<int> &dp){
+    //     if(idx==nums.size()-1)return true;
+    //     if(dp[idx]!=-1)return dp[idx];
+    //     for(int i=1;i<=nums[idx];i++){
+    //         if(help(idx+i,nums,dp))return true;
+    //     }
+    //     return dp[idx]=false;
+    // }
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return help(0,nums,dp);    
+        int n=nums.size();
+        vector<int> dp(n,false);
+        dp[n-1]=true;
+
+        for(int idx=n-2;idx>=0;idx--){
+            for(int i=1;i<=nums[idx];i++){
+                if(idx+i<n && dp[idx+i])dp[idx]=true;
+            }
+        }
+
+        return dp[0];    
     }
 };
