6b56cad0f6b9551d2abce374bb6060468d666d71
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-11 13:45:55 +0530

    Time: 0 ms (100%), Space: 52.3 MB (65.39%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    // bool help(int idx,vector<int> &nums,vector<int> &dp){
-    //     if(idx==nums.size()-1)return true;
-    //     if(dp[idx]!=-1)return dp[idx];
-    //     for(int i=1;i<=nums[idx];i++){
-    //         if(help(idx+i,nums,dp))return true;
-    //     }
-    //     return dp[idx]=false;
-    // }
     bool canJump(vector<int>& nums) {
+        int maxRechableIdx=0;
         int n=nums.size();
-        vector<int> dp(n,false);
-        dp[n-1]=true;
-
-        for(int idx=n-2;idx>=0;idx--){
-            for(int i=1;i<=nums[idx];i++){
-                if(idx+i<n && dp[idx+i])dp[idx]=true;
-            }
-        }
-
-        return dp[0];    
+        for(int i=0;i<n;i++){
+            if(i>maxRechableIdx)return false;
+            maxRechableIdx=max(maxRechableIdx,i+nums[i]);   
+        }  
+        return true;    
     }
 };
