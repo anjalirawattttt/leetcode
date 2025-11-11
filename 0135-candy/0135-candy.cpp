c6995d62172cab66513a721c80a16e492c9fd27e
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-11 15:46:03 +0530

    Time: 0 ms (100%), Space: 22.8 MB (69.62%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,28 +3,20 @@ public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
         vector<int> candies(n,1);
-        //left to right pass
         int i=1;
         while(i<n){
-            if(ratings[i-1]<ratings[i]){
-                candies[i]=candies[i-1]+1;
-            }
+            if(ratings[i]>ratings[i-1])candies[i]=candies[i-1]+1;
             i++;
         }
-        //right to left pass
         i=n-2;
         while(i>=0){
-            if(ratings[i]>ratings[i+1]){
-                candies[i]=max(candies[i],candies[i+1]+1);
-            }
+            if(ratings[i]>ratings[i+1])candies[i]=max(candies[i],candies[i+1]+1);
             i--;
         }
-        int ans=0;
-        for(int j=0;j<n;j++){
-            ans+=candies[j];
+        int totalCandies=0;
+        for(int i=0;i<n;i++){
+            totalCandies+=candies[i];
         }
-        return ans;
-
-
+        return totalCandies;    
     }
 };
