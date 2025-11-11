b8fa920baec09901b5dc502141643965a30216b9
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-11 11:22:50 +0530

    Time: 0 ms (100%), Space: 71.5 MB (69.79%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int help(vector<int>&nums,int k){
-        //no of subarrays with <=k odd nos
+    int help(vector<int>& nums,int k){
         if(k<0)return 0;
         int l=0,r=0,n=nums.size();
-        int odds=0,ans=0;
+        int ans=0,count=0;
         while(r<n){
-            if(nums[r]%2!=0)odds++;
-            while(odds>k){
-                if(nums[l]%2!=0)odds--;
+            if(nums[r]%2==1)count++;
+            while(l<n && count>k){
+                if(nums[l]%2==1)count--;
                 l++;
             }
-            if(odds<=k)ans+=r-l+1;
+            if(count<=k)ans+=r-l+1;
             r++;
         }
         return ans;
     }
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return help(nums,k)-help(nums,k-1);
+        return help(nums,k)-help(nums,k-1);    
     }
 };
