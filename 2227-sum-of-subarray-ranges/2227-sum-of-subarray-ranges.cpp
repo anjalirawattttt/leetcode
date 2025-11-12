1658166d68befeb4751e3252f74ebeb60f53ece3
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-12 11:08:28 +0530

    Time: 0 ms (100%), Space: 15.4 MB (66.78%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
+    long long subArrayRanges(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        long long maxSum=0,minSum=0;  
+        stack<int> s; 
+        //CALCULATE MINSUM 
+        for(int i=0;i<n;i++){
+            while(!s.empty() && arr[s.top()]>arr[i])s.pop();
+            left[i]=s.empty()?i+1:i-s.top();
+            s.push(i);
+        }
+        while(!s.empty())s.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && arr[s.top()]>=arr[i])s.pop();
+            right[i]=s.empty()?n-i:s.top()-i;
+            s.push(i);
+        }
+        for(int i=0;i<n;i++){
+            minSum=(minSum+(1LL*arr[i]*left[i]*right[i]));
+        }
+        //CALCULATE MAXSUM
+        while(!s.empty())s.pop();
+        for(int i=0;i<n;i++){
+            while(!s.empty() && arr[s.top()]<arr[i])s.pop();
+            left[i]=s.empty()?i+1:i-s.top();
+            s.push(i);
+        }
+        while(!s.empty())s.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && arr[s.top()]<=arr[i])s.pop();
+            right[i]=s.empty()?n-i:s.top()-i;
+            s.push(i);
+        }
+        for(int i=0;i<n;i++){
+            maxSum=(maxSum+(1LL*arr[i]*left[i]*right[i]));
+        }
         long long ans=0;
-        int n=nums.size();
-        vector<int> pse(n);
-        vector<int> nse(n);
-        vector<int> pge(n);
-        vector<int> nge(n);
-
-
-        stack<int> s;
-        for(int i=0;i<n;i++){
-            while(!s.empty() && nums[s.top()]>nums[i])s.pop();
-            pse[i]=s.empty()?i+1:i-s.top();
-            s.push(i);
-        }
-        while(!s.empty())s.pop();
-        for(int i=n-1;i>=0;i--){
-            while(!s.empty() && nums[s.top()]>=nums[i])s.pop();
-            nse[i]=s.empty()?n-i:s.top()-i;
-            s.push(i);
-        }
-        while(!s.empty())s.pop();
-
-
-        for(int i=0;i<n;i++){
-            while(!s.empty() && nums[s.top()]<nums[i])s.pop();
-            pge[i]=s.empty()?i+1:i-s.top();
-            s.push(i);
-        }
-        while(!s.empty())s.pop();
-        for(int i=n-1;i>=0;i--){
-            while(!s.empty() && nums[s.top()]<=nums[i])s.pop();
-            nge[i]=s.empty()?n-i:s.top()-i;
-            s.push(i);
-        }
-
-        for(int i=0;i<n;i++){
-            ans += nums[i]*( ((long long)nge[i]*pge[i]) - ((long long)nse[i]*pse[i]) );
-        }
-    
-
+        ans=maxSum-minSum;
         return ans;   
     }
 };
