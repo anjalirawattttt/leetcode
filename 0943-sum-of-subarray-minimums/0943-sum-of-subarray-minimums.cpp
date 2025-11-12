03db5bc928e435a9527afab2a13caaeffa3bd265
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-12 11:02:14 +0530

    Time: 15 ms (69.62%), Space: 47.1 MB (62.69%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -3,24 +3,25 @@ public:
     const int MOD=1e9+7;
     int sumSubarrayMins(vector<int>& arr) {
         int n=arr.size();
-        long long ans=0;
-        stack<int> s;
-        vector<int> left(n,1);
-        vector<int> right(n,1);
+        vector<int> left(n);
+        vector<int> right(n);  
+        stack<int> s;  
         for(int i=0;i<n;i++){
             while(!s.empty() && arr[s.top()]>arr[i])s.pop();
-            left[i]=s.empty() ? i+1 :i-s.top();
+            left[i]=s.empty()?i+1:i-s.top();
             s.push(i);
         }
         while(!s.empty())s.pop();
         for(int i=n-1;i>=0;i--){
             while(!s.empty() && arr[s.top()]>=arr[i])s.pop();
-            right[i]=s.empty() ? n-i : s.top()-i;
+            right[i]=s.empty()?n-i:s.top()-i;
             s.push(i);
         }
+
+        long long ans=0;
         for(int i=0;i<n;i++){
             ans=(ans+(1LL*arr[i]*left[i]*right[i])%MOD)%MOD;
         }
-        return (int)ans;    
+        return (int)ans;
     }
 };
