c010e778a81fb31c938de84dac49531df604f2c0
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-13 16:52:12 +0530

    Time: 0 ms (100%), Space: 13.3 MB (35.3%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,21 +3,29 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m=triangle.size();
         int n=triangle[m-1].size();
-        vector<int> dp(n,-1);
+        vector<vector<int>> dp;
+        for(int i=0;i<m;i++){
+            dp.push_back(vector<int> (i+1,-1));
+        }
 
-
-
-        for(int i=m-1;i>=0;i--){
+        for(int i=0;i<m;i++){
             for(int j=0;j<triangle[i].size();j++){
-                if(i==m-1){
-                    dp[j]=triangle[i][j];
+                if(i==0 && j==0){
+                    dp[i][j]=triangle[i][j];
                 }
                 else{
-                    dp[j]=triangle[i][j]+min(dp[j],dp[j+1]);
+                    int top=INT_MAX,tl=INT_MAX;
+                    if(i>0 && j>0)tl=dp[i-1][j-1];
+                    if(i>0 && j<i)top=dp[i-1][j];
+                    dp[i][j]=triangle[i][j]+min(top,tl);
                 }
             }
         }
 
-        return dp[0];
+        int ans=INT_MAX;
+        for(int i=0;i<dp[m-1].size();i++){
+            ans=min(ans,dp[m-1][i]);
+        }
+        return ans;
     }
 };
