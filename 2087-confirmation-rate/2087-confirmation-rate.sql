bf001edc3948c4bb31f83f61b3e8b146b32009c9
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-15 16:15:27 +0530

    Time: 610 ms (62.63%), Space: 0B (100%) - LeetHub

---
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -0,0 +1,14 @@
+SELECT s.user_id , 
+IFNULL(
+    ROUND(
+    COUNT(CASE WHEN c.action='confirmed' THEN 1 END)/
+    NULLIF(
+        COUNT(c.action)
+        ,0)
+    ,2)
+    ,0
+)
+AS confirmation_rate
+FROM Signups s LEFT JOIN Confirmations c
+ON s.user_id=c.user_id
+GROUP BY s.user_id;
