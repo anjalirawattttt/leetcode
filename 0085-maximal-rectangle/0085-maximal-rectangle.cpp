941d4e0428424574171d7605d50fd58e42b687d2
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-15 12:37:37 +0530

    Time: 13 ms (24.04%), Space: 17.7 MB (81.04%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    int maxArea(vector<int>& heights){
+    int findLargestAreaInHistogram(vector<int> &heights){
         int n=heights.size();
         stack<int> s;
-        int ans=0;
+        int maxArea=0;
         for(int i=0;i<=n;i++){
-           int height=(i==n)?0:heights[i];
-           while(!s.empty() && height<heights[s.top()]){
+            int height = i==n ? 0 : heights[i];
+            while(!s.empty() && heights[s.top()]>height){
                 int h=heights[s.top()];
                 s.pop();
-                int width=s.empty()?i:i-s.top()-1;
-                ans=max(ans,h*width);
-           }
-           s.push(i); 
+                //prev smaller element
+                int w = s.empty() ? i : i-s.top()-1;
+                maxArea=max(maxArea,h*w);
+            }
+            s.push(i);
         }
-        return ans;
+        return maxArea;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if(matrix.empty())return 0;
         int m=matrix.size();
         int n=matrix[0].size();
-        vector<int> heights(n,0);
-        int ans=0;
-
+        vector<int> currRow(n,0);
+        int maxArea=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]=='0')heights[j]=0;
-                else heights[j]++;
+                if(matrix[i][j]=='0')currRow[j]=0;
+                else currRow[j]++;
             }
-            int area=maxArea(heights);
-            ans=max(ans,area);  
+            int area=findLargestAreaInHistogram(currRow);
+            cout<<area<<endl;
+            maxArea=max(maxArea,area);
         }
-        return ans;   
+        return maxArea;
     }
 };
