d3cbc9295fd35406e29a311623778e1d7ede297d
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-16 16:32:10 +0530

    Time: 0 ms (100%), Space: 9 MB (38.75%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,45 +1,60 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int n1=a.length(),n2=b.length();
-        if(n2>n1)return addBinary(b,a);
         reverse(a.begin(),a.end());
         reverse(b.begin(),b.end());
+        int na=a.size(),nb=b.size();
+        int ia=0,ib=0;
         int carry=0;
-        int i=0,j=0;
         string ans="";
-        while(i<n1 && j<n2){
-            if(carry){
-                if((a[i]=='0' && b[i]=='1') || (a[i]=='1' && b[i]=='0')){
-                    ans+='0';
-                }
-                else if(a[i]=='0' && b[i]=='0'){
+        while(ia<na && ib<nb){
+            if(a[ia]=='1' && b[ib]=='1'){
+                if(carry){
                     ans+='1';
-                    carry=0;
-                }
-                else{
-                    ans+='1';
-                }
-            }
-            else{
-                if((a[i]=='0' && b[i]=='1') || (a[i]=='1' && b[i]=='0')){
-                    ans+='1';
-                }
-                else if(a[i]=='0' && b[i]=='0'){
-                    ans+='0';
+                    carry=1;
                 }
                 else{
                     ans+='0';
                     carry=1;
                 }
             }
-            i++;
-            j++;
+            else if((a[ia]=='1' && b[ib]=='0') || (a[ia]=='0' && b[ib]=='1')){
+                if(carry){
+                    ans+='0';
+                    carry=1;
+                }
+                else{
+                    ans+='1';
+                    carry=0;
+                }
+            }
+            else{
+                if(carry){
+                    ans+='1';
+                    carry=0;
+                }
+                else{
+                    ans+='0';
+                    carry=0;
+                }
+            }
+            ia++;
+            ib++;
         }
 
-        while(i<n1){
-            if(carry){
-                if(a[i]=='0'){
+        while(ia<na){
+            if(a[ia]=='1'){
+                if(carry){
+                    ans+='0';
+                    carry=1;
+                }
+                else{
+                    ans+='1';
+                    carry=0;
+                }
+            }
+            else{
+                if(carry){
                     ans+='1';
                     carry=0;
                 }
@@ -47,15 +62,31 @@ public:
                     ans+='0';
                 }
             }
-            else{
-                ans+=a[i];
+            ia++;
+        }  
+
+        while(ib<nb){
+            if(b[ib]=='1'){
+                if(carry){
+                    ans+='0';
+                }
+                else{
+                    ans+='1';
+                }
             }
-            i++;
-        }
-
+            else{
+                if(carry){
+                    ans+='1';
+                    carry=0;
+                }
+                else{
+                    ans+='0';
+                }
+            }
+            ib++;
+        } 
         if(carry)ans+='1';
-
         reverse(ans.begin(),ans.end());
-        return ans;
+        return ans;   
     }
 };
