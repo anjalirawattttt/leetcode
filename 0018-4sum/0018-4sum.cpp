a03498f01b8551c8d621952973274c05dcc4573b
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-12-23 22:57:17 +0530

    Time: 19 ms (71.11%), Space: 17.3 MB (82.35%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,31 +4,31 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         int n=nums.size();
-        if(n<4)return ans;
         for(int i=0;i<=n-4;i++){
             if(i>0 && nums[i]==nums[i-1])continue;
             for(int j=i+1;j<=n-3;j++){
                 if(j>i+1 && nums[j]==nums[j-1])continue;
-                int l=j+1,r=n-1;
-                while(l<r){
-                    long long sum=(long long)nums[i]+nums[j]+nums[l]+nums[r];
-                    if(sum==target){
-                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        while(l<r && nums[l]==nums[l+1])l++;
-                        while(l<r && nums[r]==nums[r-1])r--;
-                        l++;
-                        r--;
+                int k=j+1,l=n-1;
+                while(k<l){
+                    if((long long)nums[i]+nums[j]+nums[k]+nums[l]==target){
+                        // cout<<i<<j<<k<<l;
+                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        while(k<n-1 && nums[k]==nums[k+1]){
+                            // cout<<nums[k]<<" "<<nums[k+1]<<endl;
+                            k++;
+                        }
+                        k++;
+                        l=n-1;
                     }
-                    else if(sum<target){
-                        l++;
+                    else if((long long)nums[i]+nums[j]+nums[k]+nums[l]<target){
+                        k++;
                     }
                     else{
-                        r--;
+                        l--;
                     }
                 }
             }
         }
-
-        return ans;
+        return ans;        
     }
 };
