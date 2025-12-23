a863db1c3566ffa42a82b2b8aaca80902d958b78
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-12-23 15:15:52 +0530

    Time: 0 ms (100%), Space: 9.4 MB (50.51%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    void help(int top,int bottom,int left,int right,vector<vector<int>> &matrix,vector<int>& ans){
-        if(left>right || top>bottom)return;
-        //left to right
+    void help(vector<int> &ans,int top,int bottom,int left ,int right,vector<vector<int>> &matrix){
+        if(top>bottom || left>right)return;
         for(int j=left;j<=right;j++){
             ans.push_back(matrix[top][j]);
         }
         top++;
-        //top to bottom
-        if(top<=bottom){
+
+        if(top<=bottom && left<=right){
             for(int i=top;i<=bottom;i++){
                 ans.push_back(matrix[i][right]);
             }
             right--;
         }
-        //right to left
+
         if(top<=bottom && left<=right){
             for(int j=right;j>=left;j--){
                 ans.push_back(matrix[bottom][j]);
             }
             bottom--;
         }
-        //bottom to top
+
         if(top<=bottom && left<=right){
             for(int i=bottom;i>=top;i--){
                 ans.push_back(matrix[i][left]);
             }
+            
             left++;
         }
-        help(top,bottom,left,right,matrix,ans);
+
+        help(ans,top,bottom,left,right,matrix);
     }
+
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> ans;
         int m=matrix.size();
         int n=matrix[0].size();
-        help(0,m-1,0,n-1,matrix,ans);
+        help(ans,0,m-1,0,n-1,matrix);
         return ans;    
     }
 };
