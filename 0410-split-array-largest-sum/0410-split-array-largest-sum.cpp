f85a9078f29549a1e6d83a020aee2c1e6d8b0f3f
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-19 23:07:05 +0530

    Time: 0 ms (100%), Space: 10.4 MB (89.56%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -2,29 +2,40 @@ class Solution {
 public:
     int splitArray(vector<int>& nums, int k) {
         int n=nums.size();
-        int maxEle=0,sum=0;
-
+        int sum=0;
+        int maxEle=nums[0];
         for(int i=0;i<n;i++){
-            maxEle=max(maxEle,nums[i]);
             sum+=nums[i];
+            maxEle=max(maxEle,nums[i]);
         }
 
-        int l=maxEle,r=sum;
-        while(l!=r){
-            int mid=l+(r-l)/2;
-            int sub=1,temp=0;
+        int ans=-1;
 
+    
+        int l=maxEle,r=sum;
+        while(l<=r){
+            int mid = l + (r-l)/2 ;
+            int s=0,bags=0;
             for(int i=0;i<n;i++){
-                if(temp+nums[i]<=mid)temp+=nums[i];
-                else {
-                    sub++;
-                    temp=nums[i];
+                if(s+nums[i]<=mid){
+                    s+=nums[i];
+                }
+                else{
+                    bags++;
+                    s=nums[i];
                 }
             }
-            if(sub<=k)r=mid;
-            else l=mid+1;
+            bags++;
 
+            if(bags<=k){
+                ans=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
         }
-        return l;
+        return ans;
+
     }
 };
