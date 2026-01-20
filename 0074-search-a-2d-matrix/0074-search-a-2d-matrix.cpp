c17bdd470b06b62e19e7a137c211f73c326f9974
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-20 17:03:42 +0530

    Time: 0 ms (100%), Space: 13.3 MB (76.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,29 +3,37 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size();
         int n=matrix[0].size();
-        int l=0,h=m-1;
-        int r=-1;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(target>=matrix[mid][0] && target<=matrix[mid][n-1]){
-                r=mid;
-                break;
+
+        int rl=0,rh=m-1;
+        int cl=0,ch=n-1;
+        while(rl<=rh && cl<=ch){
+            int rmid = rl + (rh-rl)/2;
+            int cmid = cl + (ch-cl)/2;
+            if(matrix[rmid][cmid]==target)return true;
+            else if(matrix[rmid][cmid]<target){
+                if(matrix[rmid][n-1]<target){
+                    rl=rmid+1;
+                    cl=0;
+                    ch=n-1;
+                }
+                else{
+                    cl=cmid+1;
+                }
+            } 
+            else{
+                //>
+                if(matrix[rmid][0]>target){
+                    rh=rmid-1;
+                    cl=0;
+                    ch=n-1;
+                }
+                else{
+                    ch=cmid-1;
+                }
             }
-            else if(target<matrix[mid][0]){
-                h=mid-1;
-            }
-            else if(target>matrix[mid][n-1]){
-                l=mid+1;
-            }
-        } 
-        if(r==-1)return false;
-        l=0;h=n-1;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(matrix[r][mid]==target)return true;
-            else if(matrix[r][mid]>target)h=mid-1;
-            else l=mid+1;
-        } 
-        return false;  
+        }
+        return false;
+
+        
     }
 };
