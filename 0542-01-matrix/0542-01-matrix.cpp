e50645ec1d74dd717fbb906854f803ce41cb60b5
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-23 10:57:44 +0530

    Time: 2581 ms (5.02%), Space: 33.9 MB (96.77%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
+    vector<int> rows={1,-1,0,0};
+    vector<int> cols={0,0,1,-1};
+    void dfs(int i,int j,vector<vector<int>> &mat,vector<vector<int>> &dist){
+        for(int k=0;k<4;k++){
+            int r=i+rows[k];
+            int c=j+cols[k];
+            if(r>=0 && c>=0 && r<mat.size() && c<mat[0].size() && mat[r][c]==1 && dist[r][c]>dist[i][j]+1){
+                dist[r][c]=(dist[i][j]+1);
+                dfs(r,c,mat,dist);
+            }
+        }
+    }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
-        vector<vector<int>> dist(m,vector<int>(n,-1));
-        // multi source bfs
-        queue<pair<int,int>> q;
+        vector<vector<int>> dist(m,vector<int> (n,INT_MAX));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
                     dist[i][j]=0;
-                    q.push({i,j});    
+                    dfs(i,j,mat,dist);
                 }
             }
         }
-
-        vector<int> rows={1,-1,0,0};
-        vector<int> cols={0,0,1,-1};
-        while(!q.empty()){
-            auto [i,j]=q.front();
-            q.pop();
-            for(int k=0;k<4;k++){
-                int nr=i+rows[k];
-                int nc=j+cols[k];
-                if(nr>=0 && nc>=0 && nr<m && nc<n && dist[nr][nc]==-1){
-                    dist[nr][nc]= dist[i][j]+1;
-                    q.push({nr,nc});
-                }
-            }
-        }  
         return dist;
+        
     }
 };
