1ae60649f9b9f96bf359ecb5334fda8489d80df7
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-23 15:43:23 +0530

    Time: 309 ms (30.07%), Space: 19.8 MB (81.66%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,31 +1,67 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> s;
-        for(string &word:wordList){
-            s.insert(word);
-        }   
-
-        if(s.find(endWord)==s.end())return 0;
-
-        queue<pair<string,int>> q;//word+steps
-        q.push({beginWord,1});
-        while(!q.empty()){
-            auto[word,steps]=q.front();
-            q.pop();
-            if(word==endWord)return steps;
-
-            for(int i=0;i<word.length();i++){
-                string temp=word;
-                for(char ch='a';ch<='z';ch++){
-                    temp[i]=ch;
-                    if(s.find(temp)!=s.end()){
-                        q.push({temp,steps+1});
-                        s.erase(temp);
+    int help(string src,string dst,set<string> &s,int moves,map<string,int> &m){
+        if(src==dst){
+            return moves;
+        }
+        m[src]=moves;
+        int ans=INT_MAX;
+        for(int i=0;i<src.length();i++){
+            char ch=src[i];
+            for(char c='a';c<='z';c++){
+                if(ch==c)continue;
+                src[i]=c;
+                if(s.find(src)!=s.end()){
+                    if(m[src]!=0 && m[src]<moves+1){
+                        continue;
                     }
+                    s.erase(src);
+                    ans=min(ans,help(src,dst,s,moves+1,m));
+                    s.insert(src);
                 }
             }
+            src[i]=ch;
         }
-        return 0;
+        return ans;
+    }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        set<string> s;
+        for(int i=0;i<wordList.size();i++){
+            s.insert(wordList[i]);
+        } 
+
+        if(s.find(endWord)==s.end())return 0;
+        if(beginWord.length()!=endWord.length())return 0;
+
+        int ans=INT_MAX;
+        queue<string> q; 
+        q.push(beginWord);
+        int moves=1;
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                string curr=q.front();
+                q.pop();
+                if(curr==endWord){
+                    ans=moves; 
+                    break;   
+                }
+                for(int i=0;i<curr.length();i++){
+                    char temp=curr[i];
+                    for(char ch='a';ch<='z';ch++){
+                        if(ch==temp)continue;
+                        curr[i]=ch;
+                        if(s.find(curr)!=s.end()){
+                            s.erase(curr);
+                            q.push(curr);
+                        }
+                    }
+                    curr[i]=temp;
+                }
+            }
+            moves++;
+        }
+        if(ans==INT_MAX)return 0;
+        return ans;  
     }
 };
