f8dc6ffdc56e5cc8e4c5f3e4f4a533fe3cc58ad7
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-24 09:59:00 +0530

    Time: 11 ms (23.78%), Space: 19.2 MB (82.04%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,32 +2,28 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        vector<int> indegrees(numCourses,0);
-        for(auto &p:prerequisites){
-            adj[p[1]].push_back(p[0]);
-            indegrees[p[0]]++;
-        } 
+        vector<int> inDegree(numCourses,0); 
+        for(int i=0;i<prerequisites.size();i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            inDegree[prerequisites[i][0]]++;
+        }   
         queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(indegrees[i]==0)q.push(i);
+            if(inDegree[i]==0)q.push(i);
         }
+
         while(!q.empty()){
             int u=q.front();
             q.pop();
             for(int &v:adj[u]){
-                indegrees[v]--;
-                if(indegrees[v]==0)q.push(v);
+                inDegree[v]--;
+                if(inDegree[v]==0)q.push(v);
             }
         }
 
         for(int i=0;i<numCourses;i++){
-            if(indegrees[i]!=0)return false;
+            if(inDegree[i]!=0)return false;
         }
-
         return true;
-
-
-
-
     }
 };
