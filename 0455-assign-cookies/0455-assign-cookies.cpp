1571a59d17d5d10769573af72a3e62ea8e94f398
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-28 18:51:48 +0530

    Time: 16 ms (11.05%), Space: 46 MB (11.28%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
+    bool static cmp(const int &a,const int &b){
+        return a>b;
+    }
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int ans=0;
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
+        sort(g.begin(),g.end(),cmp);
+        sort(s.begin(),s.end(),cmp);
+
+        int size_g=g.size();
+        int size_s=s.size();
 
         int i=0,j=0;
-        while(i<g.size() && j<s.size()){
-            if(s[j]>=g[i]){
-                ans++;
+
+        while( i < size_s && j < size_g ){
+            if(s[i]>=g[j]){
                 i++;
                 j++;
             }
             else{
                 j++;
             }
-        }
-        return ans;
+        }    
 
-
+        return i;
     }
 };
