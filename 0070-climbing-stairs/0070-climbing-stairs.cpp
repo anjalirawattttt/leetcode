5499b83e12f9c1717a365caeecfed41bfd8405a3
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-28 10:27:40 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int help(int n,vector<int> &dp){
-        if(n==0)return 1;
-        if(n<0)return 0;
-        if(dp[n]!=-1)return dp[n];
-        int op1=help(n-1,dp);
-        int op2=help(n-2,dp);
-        return dp[n]=op1+op2;
-    }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return help(n,dp);    
+        //tabulation
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            int op1=dp[i-1];
+            int op2=0;
+            if(i>1)op2=dp[i-2];
+            dp[i]=op1+op2;
+        }   
+        return dp[n]; 
     }
 };
