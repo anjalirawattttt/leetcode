f6e04d5c93daa6e88babe91ad8a766f40e92e02d
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-29 11:24:34 +0530

    Time: 92 ms (29.11%), Space: 76 MB (42.78%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    // int help(int i1,int i2,string s1,string s2,vector<vector<int>>& dp){
-    //     if(i1<0 || i2<0)return 0;
-    //     if(dp[i1][i2]!=-1)return dp[i1][i2];
-    //     //match
-    //     if(s1[i1]==s2[i2]){
-    //         return dp[i1][i2]=1+help(i1-1,i2-1,s1,s2,dp);
-    //     }
-    //     //not match
-    //     return dp[i1][i2]=max(help(i1-1,i2,s1,s2,dp),help(i1,i2-1,s1,s2,dp));
-    // }
-    int longestPalindromeSubseq(string s) {
-        int n=s.length();
-        string s2="";
-        for(int i=n-1;i>=0;i--){
-            s2+=s[i];
+    int help(int i1,string s1,int i2,string s2,vector<vector<int>>& dp){
+        if(i1<0 || i2<0)return 0;
+        if(dp[i1][i2]!=-1)return dp[i1][i2];
+        if(s1[i1]==s2[i2]){
+            return dp[i1][i2]=1+help(i1-1,s1,i2-1,s2,dp);
         }
-        vector<int> prev(n+1,0);
+        int op1=help(i1-1,s1,i2,s2,dp);
+        int op2=help(i1,s1,i2-1,s2,dp);
+        return dp[i1][i2]=max(op1,op2);
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1=text1.length();
+        int n2=text2.length();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
 
-        for(int i1=1;i1<=n;i1++){
-            vector<int> curr(n+1,0);
-            curr[0]=0;
-            for(int i2=1;i2<=n;i2++){
-                if(s[i1-1]==s2[i2-1]){
-                    curr[i2]=1+prev[i2-1];
+        for(int i1=1;i1<=n1;i1++){
+            for(int i2=1;i2<=n2;i2++){
+                if(text1[i1-1]==text2[i2-1]){
+                    dp[i1][i2]=1+dp[i1-1][i2-1];
                 }
-                //not match
                 else{
-                    curr[i2]=max(prev[i2],curr[i2-1]);
+                    int op1=dp[i1-1][i2];
+                    int op2=dp[i1][i2-1];
+                    dp[i1][i2]=max(op1,op2);
                 }
             }
-            prev=curr;
         }
 
-
-
-        return prev[n];           
+        return dp[n1][n2];    
+    }
+    int longestPalindromeSubseq(string s) {
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        return longestCommonSubsequence(s,rev);
     }
 };
