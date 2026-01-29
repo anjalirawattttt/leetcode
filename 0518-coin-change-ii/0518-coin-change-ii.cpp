a6338fea47b5a99509a807cbda3d62503f00dfde
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-29 11:08:19 +0530

    Time: 4 ms (93.07%), Space: 10.3 MB (85.59%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int help(int i,vector<int> &coins,int amount,vector<vector<int>> &dp){
-        if(amount==0)return 1;
-        if(i<0)return 0;
-        if(dp[i][amount]!=-1)return dp[i][amount];
-        //take
-        int op1=0;
-        if(amount>=coins[i]){
-            op1=help(i,coins,amount-coins[i],dp);
-        }
-        //not take
-        int op2=help(i-1,coins,amount,dp);
-        return dp[i][amount]=  op1+op2;
-    }
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return help(n-1,coins,amount,dp);
+        vector<unsigned long long> dp(amount + 1, 0); 
+        dp[0] = 1;
+        
+        for (int c : coins) {
+            for (int a = c; a <= amount; a++) {
+                dp[a] += dp[a - c];
+            }
+        }
+        
+        return dp[amount];        
     }
 };
