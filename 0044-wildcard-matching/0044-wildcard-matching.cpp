8db567fb6459947bd57eba8bcff33beb49c8c4c3
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-30 18:29:47 +0530

    Time: 67 ms (23.48%), Space: 55.1 MB (6.11%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,54 +1,45 @@
 class Solution {
 public:
-    // bool help(int i,int j,string s,string p,vector<vector<int>>& dp){
-    //     if(i<0 && j<0)return true;
-    //     if(j<0 && i>=0)return false;
-    //     if(i<0 && j>=0){
-    //         for(int k=0;k<=j;k++){
-    //             if(p[k]!='*')return false;
-    //         }
-    //         return true;
-    //     }
-    //     if(dp[i][j]!=-1)return dp[i][j];
-        
-    //     if(p[j]=='?' || s[i]==p[j]){
-    //         return dp[i][j]=help(i-1,j-1,s,p,dp);
-    //     } 
-    //     else if(p[j]=='*'){
-    //         return dp[i][j]=help(i,j-1,s,p,dp) || help(i-1,j,s,p,dp);
-    //     }
-    //     return dp[i][j]=false;
-    // }
     bool isMatch(string s, string p) {
         int n=s.length();
         int m=p.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
-        vector<bool> prev(m+1,false);
+        dp[0][0]=1;
 
-        prev[0]=true;
-        for(int j=1;j<=m;j++){
-            prev[j]= true;
-            for(int k=1;k<=j;k++){
-                if(p[k-1]!='*')prev[j]= false;
-            }
+        int j=0;
+        while(j<m && p[j]=='*'){
+            dp[0][j+1]=1;
+            j++;
         }
+
+        vector<int> temp(m+1,0);
+        temp[0]=1;
 
         for(int i=1;i<=n;i++){
-            vector<bool> curr(m+1,false);
             for(int j=1;j<=m;j++){
-                if(p[j-1]=='?' || s[i-1]==p[j-1]){
-                    curr[j]=prev[j-1];
-                } 
-                else if(p[j-1]=='*'){
-                    curr[j]=curr[j-1] || prev[j];
+                if(s[i-1]==p[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
                 }
-                else{
-                    curr[j]=false;   
+                else if(p[j-1]=='*'){
+                    if(temp[j-1])dp[i][j]=1;
+                }
+                else if(p[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                if(dp[i][j]){
+                    temp[j]=1;
                 }
             }
-            prev=curr;
         }
-        
-        return prev[m];   
+
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=m;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+
+        return dp[n][m];
     }
 };
