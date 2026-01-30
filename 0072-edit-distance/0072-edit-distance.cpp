ed4fae14baaf0b340f7a543475a690c1544cc16d
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-01-30 17:56:14 +0530

    Time: 66 ms (9.73%), Space: 83 MB (9.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    int help(string &word1,int i1,string &word2,int i2,vector<vector<int>> &dp){
-        if(i1==0 && i2==0)return word1[i1]==word2[i2]?0:1 ;
-        if(i2<0)return i1+1;
-        if(i1<0)return i2+1;
-        if(dp[i1][i2]!=-1)return dp[i1][i2];
-
-        if(word1[i1]==word2[i2]){
-            return dp[i1][i2]=help(word1,i1-1,word2,i2-1,dp);
+    int help(int i1,string s1,int i2,string s2,vector<vector<int>> &dp){
+        if(i1<0 && i2<0)return 0;
+        if(i2<0){
+            return i1+1;
         }
+        if(i1<0){
+            return i2+1;
+        }
+
+        if(dp[i1][i2]!=-1)return dp[i1][i2];
+        if(s1[i1]==s2[i2]){
+            return dp[i1][i2]=help(i1-1,s1,i2-1,s2,dp);
+        }    
         //insert
-        int op1=1+help(word1,i1,word2,i2-1,dp);
-        //delete 
-        int op2=1+help(word1,i1-1,word2,i2,dp);
+        int op1=1+help(i1,s1,i2-1,s2,dp);
+        //delete
+        int op2=1+help(i1-1,s1,i2,s2,dp);
         //replace
-        int op3=1+help(word1,i1-1,word2,i2-1,dp);
+        int op3=1+help(i1-1,s1,i2-1,s2,dp);
         return dp[i1][i2]=min(op1,min(op2,op3));
+
     }
     int minDistance(string word1, string word2) {
-        int n1=word1.length(),n2=word2.length();
+        int n1=word1.length();
+        int n2=word2.length();
         vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return help(word1,n1-1,word2,n2-1,dp);   
+        return help(n1-1,word1,n2-1,word2,dp);    
     }
 };
