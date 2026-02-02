b1024361e0230b65410b2ff8ea0a60ce27df7588
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-02-02 11:49:52 +0530

    Time: 473 ms (11.99%), Space: 235.9 MB (6.79%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    int help(int i,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp){
-        if(i==prices.size())return 0;
-        if(cap==0)return 0;
-        if(dp[i][buy][cap]!=-1)return dp[i][buy][cap];
+    int help(int buy,int i,int totalSell,vector<int> &prices,vector<vector<vector<int>>> &dp){
+        if(i==prices.size()){
+            return 0;
+        }
+        if(totalSell==2)return 0;
+        if(dp[buy][i][totalSell]!=-1)return dp[buy][i][totalSell];
         int profit=0;
         if(buy){
-            profit = max ( -prices[i]+help(i+1,0,cap,prices,dp) , help(i+1,1,cap,prices,dp) );
+            //buy
+            profit = max(profit, -prices[i]+help(0,i+1,totalSell,prices,dp) );
+            //not buy
+            profit = max(profit, help(buy,i+1,totalSell,prices,dp) );
         }
         else{
-            profit = max ( +prices[i]+help(i+1,1,cap-1,prices,dp) , help(i+1,0,cap,prices,dp) );
+            //sell
+            int s = prices[i]+help(1,i+1,totalSell+1,prices,dp);
+            profit = max(profit,s);
+            //not sell
+            int ns = help(buy,i+1,totalSell,prices,dp) ;
+            profit = max(profit,ns);
         }
-        return dp[i][buy][cap]=profit;
+        return dp[buy][i][totalSell]=profit;
     }
-
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return help(0,1,2,prices,dp);
-        
+        int n=prices.size();   
+        vector<vector<vector<int>>> dp(2,vector<vector<int>>(n,vector<int>(3,-1)));     
+        return help(1,0,0,prices,dp);    
     }
 };
