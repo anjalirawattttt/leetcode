2a33f1411fcda71be4bff76d172a956eae3f9591
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-02-04 11:31:35 +0530

    Time: 96 ms (73.36%), Space: 15.3 MB (7.53%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    long long help(int i,int j,vector<int> &nums,vector<vector<long long>>  &dp){
-        if(i>j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        long long maxCoins=INT_MIN;
-        for(int k=i;k<=j;k++){
-            long long coins = (nums[i-1]*nums[k]*nums[j+1]) + help(i,k-1,nums,dp) + help(k+1,j,nums,dp) ;
-            maxCoins=max(maxCoins,coins); 
-        }
-        // cout << i << "," << j << "->" << maxCoins << endl;
-        return dp[i][j]=maxCoins;
-    }
+    // long long help(int i,int j,vector<int> &nums,vector<vector<long long>>  &dp){
+    //     if(i>j)return 0;
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     long long maxCoins=INT_MIN;
+    //     for(int k=i;k<=j;k++){
+    //         long long coins = (nums[i-1]*nums[k]*nums[j+1]) + help(i,k-1,nums,dp) + help(k+1,j,nums,dp) ;
+    //         maxCoins=max(maxCoins,coins); 
+    //     }
+    //     // cout << i << "," << j << "->" << maxCoins << endl;
+    //     return dp[i][j]=maxCoins;
+    // }
     int maxCoins(vector<int>& nums) {
         nums.push_back(1);
         nums.insert(nums.begin(),1);
         int n=nums.size();
-        vector<vector<long long>> dp(n,vector<long long> (n,-1));
+        vector<vector<long long>> dp(n,vector<long long> (n,0));
         //last guy 
-        // for(int i=){
-        //     for(int j=){
-        //         long long maxCoins=INT_MIN;
-        //         for(int k=i;k<=j;k++){
-        //             long long coins = (nums[i-1]*nums[k]*nums[j+1]) + dp[i][k-1] + dp[k+1][j] ;
-        //             maxCoins=max(maxCoins,coins); 
-        //         }
-        //         dp[i][j]=maxCoins;
-        //     }
-        // }
-        return help(1,n-2,nums,dp);    
+        for(int i=n-2;i>=1;i--){
+            for(int j=i;j<=n-2;j++){
+                long long maxCoins=INT_MIN;
+                for(int k=i;k<=j;k++){
+                    long long coins = (nums[i-1]*nums[k]*nums[j+1]) + dp[i][k-1] + dp[k+1][j] ;
+                    maxCoins=max(maxCoins,coins); 
+                }
+                dp[i][j]=maxCoins;
+            }
+        }
+        return dp[1][n-2];    
     }
 };
