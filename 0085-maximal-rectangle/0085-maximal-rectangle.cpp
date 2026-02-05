c6067daabf8635e411a34dbd523b62f95ce97283
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-02-05 16:15:56 +0530

    Time: 1046 ms (5.03%), Space: 22 MB (10.13%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,36 +1,61 @@
 class Solution {
 public:
-    int findLargestAreaInHistogram(vector<int> &heights){
+    int lah(vector<int> &heights){
         int n=heights.size();
+        vector<int> lse(n);
+        vector<int> rse(n);
+
         stack<int> s;
-        int maxArea=0;
-        for(int i=0;i<=n;i++){
-            int height = i==n ? 0 : heights[i];
-            while(!s.empty() && heights[s.top()]>height){
-                int h=heights[s.top()];
-                s.pop();
-                //prev smaller element
-                int w = s.empty() ? i : i-s.top()-1;
-                maxArea=max(maxArea,h*w);
-            }
+        s.push(-1);
+
+        for(int i=0;i<n;i++){
+            while(s.top()!=-1 && heights[s.top()]>=heights[i])s.pop();
+            lse[i]=s.top();
             s.push(i);
+        }
+
+        while(!s.empty())s.pop();
+        s.push(n);
+
+        for(int i=n-1;i>=0;i--){
+            while(s.top()!=n && heights[s.top()]>=heights[i])s.pop();
+            rse[i]=s.top();
+            s.push(i);
+        }
+
+        for(int i=0;i<n;i++)cout<<lse[i]<<" ";
+        cout<<endl;
+        for(int i=0;i<n;i++)cout<<rse[i]<<" ";
+        cout<<endl;
+        for(int i=0;i<n;i++)cout<<heights[i]<<" ";
+        cout<<endl;
+
+        int maxArea=0;
+        for(int i=0;i<n;i++){
+            int height=heights[i];
+            int width=rse[i]-lse[i]-1;
+            int area=height*width;
+            maxArea=max(maxArea,area);
         }
         return maxArea;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        vector<int> currRow(n,0);
-        int maxArea=0;
+        vector<int> height(n,0);
+        int ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]=='0')currRow[j]=0;
-                else currRow[j]++;
+                if(matrix[i][j]=='1'){
+                    height[j]++;
+                }  
+                else{
+                    height[j]=0;
+                }  
             }
-            int area=findLargestAreaInHistogram(currRow);
-            cout<<area<<endl;
-            maxArea=max(maxArea,area);
-        }
-        return maxArea;
+            int temp=lah(height);
+            ans=max(ans,temp);
+        }  
+        return ans;  
     }
 };
