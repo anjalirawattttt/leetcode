e961a41bed5b6966221a17bf9ea05700467ebeec
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-02-06 16:52:29 +0530

    Time: 0 ms (100%), Space: 28.2 MB (62.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();  
         int candidate=-1;
-        int count=0;
+        int freq=0; 
         for(int i=0;i<n;i++){
             if(candidate==-1){
                 candidate=nums[i];
-                count++;
-                continue;
+                freq=1;
             }
-            if(nums[i]==candidate){
-                count++;
+            else if(nums[i]==candidate){
+                freq++;
             }
             else{
-                count--;
-                if(count==0)candidate=-1;
+                freq--;
             }
-        }    
+            if(freq==0){
+                candidate=-1;
+            }
+        } 
         return candidate;
     }
 };
