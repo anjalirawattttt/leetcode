b811e1fa7519d8e968c15e3ff7056d1adda99a61
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-02-12 09:45:17 +0530

    Time: 3 ms (53.33%), Space: 28.2 MB (57.03%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
-    bool isSafe(int r,int c,int m,int n){
-        return r>=0 && c>=0 && r<m && c<n;
-    }
-
-    void dfsRec(vector<vector<int>>& grid,vector<vector<bool>>& visited,int i,int j,int &area,int &ans){
-        if(i==grid.size() || j==grid[0].size())return;
-        vector<int> rows={1,-1,0,0};
-        vector<int> cols={0,0,1,-1};
+    vector<int> rows={1,-1,0,0};
+    vector<int> cols={0,0,1,-1};
+    int dfs(int i,int j,vector<vector<int>> &grid,vector<vector<bool>> &visited){
+        visited[i][j]=true;
+        int area=1;
         for(int k=0;k<4;k++){
-            int r=i+rows[k];
-            int c=j+cols[k];
-            if(isSafe(r,c,grid.size(),grid[0].size()) && visited[r][c]==false){
-                if(grid[r][c]==1){
-                    area++;
-                    visited[r][c]=true;
-                    dfsRec(grid,visited,r,c,area,ans);
-                }
-            }
+            int r = i + rows[k];
+            int c = j + cols[k];
+            if(r>=0 && c>=0 && r<grid.size() && c<grid[0].size() && grid[r][c]==1 && !visited[r][c]){
+                area+=dfs(r,c,grid,visited);
+            } 
         }
-        ans=max(ans,area);
+        return area;
     }
-
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int ans=0;
         int m=grid.size();
         int n=grid[0].size();
+        int maxArea=0;
         vector<vector<bool>> visited(m,vector<bool>(n,false));
-        int area;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(visited[i][j]==false && grid[i][j]==1){
-                    area=1;
-                    visited[i][j]=true;
-                    dfsRec(grid,visited,i,j,area,ans);
+                if(grid[i][j]==1 && !visited[i][j]){
+                    int area=dfs(i,j,grid,visited);
+                    maxArea=max(maxArea,area);
                 }
             }
-        }
-        return ans;    
+        }  
+        return maxArea;  
     }
 };
