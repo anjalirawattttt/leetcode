a701fff9b3cf1526dfe05c637cc4dd377ce891da
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-02-12 18:44:42 +0530

    Time: 4 ms (78.95%), Space: 22.2 MB (79.78%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,36 +1,55 @@
 class Solution {
 public:
-    void help(int i,int j,vector<vector<bool>> &visited,vector<vector<int>>& heights){
+    vector<int> rows={1,-1,0,0};
+    vector<int> cols={0,0,1,-1};
+    void dfs(int i,int j,vector<vector<int>> & heights,vector<vector<bool>> &visited){
         visited[i][j]=true;
-        vector<int> rows={1,-1,0,0};
-        vector<int> cols={0,0,1,-1};
         for(int k=0;k<4;k++){
-            int r=i+rows[k];
-            int c=j+cols[k];
+            int r = i + rows[k];
+            int c = j + cols[k];
             if(r>=0 && c>=0 && r<heights.size() && c<heights[0].size() && !visited[r][c] && heights[r][c]>=heights[i][j]){
-                help(r,c,visited,heights);
+                dfs(r,c,heights,visited);
             }
         }
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        vector<vector<int>> ans;
         int m=heights.size();
         int n=heights[0].size();
-        vector<vector<bool>> vp(m,vector<bool>(n,false));
-        vector<vector<bool>> va(m,vector<bool>(n,false));
-        vector<vector<int>> ans;
-        for(int i=0;i<m;i++){
-            if(!vp[i][0])help(i,0,vp,heights);
-            if(!va[i][n-1])help(i,n-1,va,heights);
-        }
+        vector<vector<bool>> pacific(m,vector<bool>(n,false));
+        //pacific top
         for(int j=0;j<n;j++){
-            if(!vp[0][j])help(0,j,vp,heights);
-            if(!va[m-1][j])help(m-1,j,va,heights);
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(vp[i][j] && va[i][j])ans.push_back({i,j});
+            if(!pacific[0][j]){
+                dfs(0,j,heights,pacific);
             }
         }
-        return ans;          
+        //pacific left
+        for(int i=0;i<m;i++){
+            if(!pacific[i][0]){
+                dfs(i,0,heights,pacific);
+            }
+        }
+        vector<vector<bool>> atlantic(m,vector<bool>(n,false));
+        //atlantic bottom
+        for(int j=0;j<n;j++){
+            if(!atlantic[m-1][j]){
+                dfs(m-1,j,heights,atlantic);
+            }
+        }
+        //atlantic right
+        for(int i=0;i<m;i++){
+            if(!atlantic[i][n-1]){
+                dfs(i,n-1,heights,atlantic);
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(pacific[i][j] && atlantic[i][j]){
+                    ans.push_back({i,j});
+                }
+            }
+        }
+        return ans;    
     }
 };
