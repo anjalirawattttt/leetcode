d987246855970c849d1d76396331d178097aa753
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-02-16 10:56:12 +0530

    Time: 3 ms (17.72%), Space: 11.9 MB (58.78%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,39 +1,50 @@
 class Solution {
 public:
-    double dfs(string &s,string &d,unordered_map<string,vector<pair<string,double>>> &m,unordered_set<string> &st){
-        if(s==d)return 1.0;
-        for(auto &p:m[s]){
-            if(st.find(p.first)==st.end()){
-                st.insert(p.first);
-                double ans=p.second*dfs(p.first,d,m,st);
-                if(ans>0.0)return ans;
-            }
-        }
-        return -1.0;
-    }
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        int n1=equations.size();
-        int n2=queries.size();
-        unordered_map<string,vector<pair<string,double>>> m;
-        for(int i=0;i<n1;i++){
-            m[equations[i][0]].push_back({equations[i][1],values[i]});
-            m[equations[i][1]].push_back({equations[i][0],1.0/values[i]});
-        } 
+        vector <double> ans;
+        int n = equations.size();   
+        unordered_map <string , vector<pair<string,double>> > adj; 
+        for( int i = 0 ; i < n ; i++ ){
+            string s1=equations[i][0];
+            string s2=equations[i][1];
+            double value=values[i];
+            adj[s1].push_back({s2,value});
+            adj[s2].push_back({s1,1.0/value});
+        }
 
-        vector<double> ans(n2);
-        for(int i=0;i<n2;i++){
-            if(m.find(queries[i][0])==m.end() || m.find(queries[i][1])==m.end()){
-                ans[i]=-1.0;
+        for( auto &query: queries){
+            string s1=query[0];
+            string s2=query[1];
+
+            if(adj.find(s1)==adj.end() || adj.find(s2)==adj.end()){
+                ans.push_back(-1.0);
                 continue;
             }
-            if(queries[i][0]==queries[i][1]){
-                ans[i]=1.0;
-                continue;
+
+            unordered_set<string> visited;
+            queue<pair<string,double>> q;
+            q.push({s1,1.0});
+            visited.insert(s1);
+            bool found=false;
+            while(!q.empty()){
+                auto [str1,v1]=q.front();
+                if(str1==s2){
+                    ans.push_back(v1);
+                    found=true;
+                    break;
+                }
+                q.pop();
+                for(auto &[str2,v2] : adj[str1]){
+                    if(visited.find(str2)==visited.end()){
+                        visited.insert(str2);
+                        q.push({str2,v1*v2});
+                    }
+                }
             }
-            unordered_set<string> st;
-            ans[i]=dfs(queries[i][0],queries[i][1],m,st);            
+            if(!found){
+                ans.push_back(-1.0);
+            }
         }
         return ans;
-
     }
 };
