f2c69244d3a81e068c2bcd22b7a3b5ebc9f6e549
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-02-16 11:50:18 +0530

    Time: 35 ms (95.65%), Space: 21.1 MB (71.17%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,67 +1,52 @@
 class Solution {
 public:
-    int help(string src,string dst,set<string> &s,int moves,map<string,int> &m){
-        if(src==dst){
-            return moves;
-        }
-        m[src]=moves;
-        int ans=INT_MAX;
-        for(int i=0;i<src.length();i++){
-            char ch=src[i];
-            for(char c='a';c<='z';c++){
+    int help (string &word, string &endWord,unordered_set<string> &words){
+        if(word==endWord)return 1;
+        int ans=1e7;
+        for(int i=0;i<word.size();i++){
+            char c=word[i];
+            for(char ch='a';ch<='z';ch++){
                 if(ch==c)continue;
-                src[i]=c;
-                if(s.find(src)!=s.end()){
-                    if(m[src]!=0 && m[src]<moves+1){
-                        continue;
-                    }
-                    s.erase(src);
-                    ans=min(ans,help(src,dst,s,moves+1,m));
-                    s.insert(src);
+                word[i]=ch;
+                if(words.find(word)!=words.end()){
+                    words.erase(word);
+                    ans=min(ans,1+help(word,endWord,words));
+                    words.insert(word);
                 }
             }
-            src[i]=ch;
+            word[i]=c;
         }
         return ans;
     }
+
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        set<string> s;
-        for(int i=0;i<wordList.size();i++){
-            s.insert(wordList[i]);
-        } 
+        unordered_set<string> words(wordList.begin(),wordList.end());
 
-        if(s.find(endWord)==s.end())return 0;
-        if(beginWord.length()!=endWord.length())return 0;
+        if(words.find(endWord)==words.end())return 0;
 
-        int ans=INT_MAX;
-        queue<string> q; 
-        q.push(beginWord);
-        int moves=1;
+        
+        queue< pair<string,int> > q;
+        q.push({beginWord,1});
+
         while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                string curr=q.front();
-                q.pop();
-                if(curr==endWord){
-                    ans=moves; 
-                    break;   
-                }
-                for(int i=0;i<curr.length();i++){
-                    char temp=curr[i];
-                    for(char ch='a';ch<='z';ch++){
-                        if(ch==temp)continue;
-                        curr[i]=ch;
-                        if(s.find(curr)!=s.end()){
-                            s.erase(curr);
-                            q.push(curr);
-                        }
+            auto [word,steps]=q.front();
+            q.pop();
+            if(word==endWord)return steps;
+            for(int i=0;i<word.size();i++){
+                char c=word[i];
+                for(char ch='a';ch<='z';ch++){
+                    if(ch==c)continue;
+                    word[i]=ch;
+                    if(words.find(word)!=words.end()){
+                        words.erase(word);
+                        q.push({word,steps+1});
                     }
-                    curr[i]=temp;
                 }
+                word[i]=c;
             }
-            moves++;
+
         }
-        if(ans==INT_MAX)return 0;
-        return ans;  
+        return 0; 
+
     }
 };
