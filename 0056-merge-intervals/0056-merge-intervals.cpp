322631a99e59fb14b9dcc80e12ceb9d8a40d7ca6
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-04-30 00:33:16 +0530

    Time: 3 ms (89.3%), Space: 23.9 MB (58.92%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
-        int start=intervals[0][0];
-        int end=intervals[0][1];
-        for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]>=start && intervals[i][0]<=end){
+        int n=intervals.size();
+        int start=intervals[0][0],end=intervals[0][1];
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=end){
                 start=min(start,intervals[i][0]);
                 end=max(end,intervals[i][1]);
             }
@@ -15,6 +15,7 @@ public:
                 start=intervals[i][0];
                 end=intervals[i][1];
             }
+
         }
         ans.push_back({start,end});
         return ans;    
