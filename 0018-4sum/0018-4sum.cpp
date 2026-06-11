83a68e0585fae1925ad80d345b04346be65ec19c
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-06-11 17:25:57 +0530

    Time: 11 ms (94.6%), Space: 17.5 MB (53.39%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        int n=nums.size();
-        for(int i=0;i<=n-4;i++){
-            if(i>0 && nums[i]==nums[i-1])continue;
-            for(int j=i+1;j<=n-3;j++){
-                if(j>i+1 && nums[j]==nums[j-1])continue;
-                int k=j+1,l=n-1;
-                while(k<l){
-                    if((long long)nums[i]+nums[j]+nums[k]+nums[l]==target){
-                        // cout<<i<<j<<k<<l;
-                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
-                        while(k<n-1 && nums[k]==nums[k+1]){
-                            // cout<<nums[k]<<" "<<nums[k+1]<<endl;
-                            k++;
-                        }
-                        k++;
-                        l=n-1;
+        int n = nums.size();
+
+        sort(nums.begin(),nums.end());
+
+        for(int i = 0 ; i<n-3 ; i++){
+            if(i > 0 && nums[i]==nums[i-1])continue;
+            for(int j = i+1 ; j<n-2 ; j++ ){
+                if(j > i+1 && nums[j]==nums[j-1])continue;
+                int l = j+1 , r = n-1;
+                while( l < r ){
+                    long long sum = (long long) nums[i] + nums[j] + nums[l] + nums[r];
+                    if( sum == target ){
+                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                        while(l<n-1 && nums[l+1]==nums[l])l++;
+                        l++;
                     }
-                    else if((long long)nums[i]+nums[j]+nums[k]+nums[l]<target){
-                        k++;
+                    else if( sum<target ){
+                        l++;
                     }
-                    else{
-                        l--;
+                    else {
+                        r--;
                     }
                 }
             }
+
         }
-        return ans;        
+        return ans;    
     }
 };
