be5465118d828a50cc5a4d9881fa235d2f903b5f
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-06-11 17:47:55 +0530

    Time: 3 ms (34.81%), Space: 77.3 MB (12.03%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,63 +10,57 @@
  */
 class Solution {
 public:
-    void help(ListNode* l1, ListNode* l2){
-        ListNode* s=l1;
-        ListNode* l=l2;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if(!l1)return l2;
+        if(!l2)return l1;
 
-        int n,carry=0;
-        while(s){
-            n=s->val + l->val + carry;
-            carry=n/10;
-            l->val=n%10;
-            s=s->next;
-            l=l->next;
-        }
-        while(carry){
-            if(l){
-                n=l->val+carry;
-                carry=n/10;
-                l->val=n%10; 
-                l=l->next;   
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2; 
+        int sum = 0 , carry = 0;
+
+        ListNode* dummy = new ListNode() ;
+        ListNode* tail = nullptr ;
+
+        while(temp1 && temp2){
+            int x=temp1->val + temp2->val + carry ;
+            sum = x % 10 ;
+            carry = x / 10 ;
+            if(!tail){
+                dummy->next = new ListNode(sum) ;
+                tail = dummy->next;
             }
             else{
-                ListNode* node=new ListNode(carry);
-                ListNode* temp=l2;
-                while(temp->next){
-                    temp=temp->next;
-                }
-                temp->next=node;
-                carry=0;               
-            }   
+                tail->next = new ListNode(sum) ;
+                tail = tail->next;
+            }
+            temp1 = temp1->next;
+            temp2 = temp2->next;
         }
-    }
-
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(!l1 || !l2)return l1 ? l1 : l2;
-
-        int a1=0,a2=0;
-        ListNode* temp1=l1;
-        ListNode* temp2=l2;
-        while(temp1){
-            a1++;
-            temp1=temp1->next;
+        while(temp1 ){
+            int x = temp1->val + carry ;
+            sum = x % 10 ;
+            carry = x / 10 ;
+            tail->next = new ListNode(sum) ;
+            tail = tail->next;
+            temp1 = temp1->next;
         }
+
         while(temp2){
-            a2++;
-            temp2=temp2->next;
+            int x = temp2->val + carry ;
+            sum = x % 10 ;
+            carry = x / 10 ;
+            tail->next = new ListNode(sum) ;
+            tail = tail->next;
+            temp2 = temp2->next;
+        }
+        
+        if(carry){
+            tail->next = new ListNode(carry) ;
+            tail = tail->next;
         }
 
-        if(a1<=a2){
-            help(l1,l2);
-        }
-        else{
-            help(l2,l1);
-        }
-
-        //temp2 is larger one
-        if(a1<=a2){
-            return l2;
-        }
-        return l1;
+        ListNode* head = dummy->next;
+        delete dummy;
+        return head;
     }
 };
