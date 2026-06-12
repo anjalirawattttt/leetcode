ef2eedf1717f0fe07a78805d4d0d8f32c8ededf4
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2026-06-12 19:24:06 +0530

    Time: 5 ms (64.09%), Space: 14.9 MB (95.94%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,34 +17,54 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(!head)return NULL;
-        
-        //create new nodes
-        Node* temp=head;
-        while(temp){
-            Node* copy=new Node(temp->val);
-            copy->next=temp->next;
-            temp->next=copy;
-            temp=copy->next;
-        }  
+        if(!head)return nullptr;
 
-        //assign random pointers
+        Node* temp = head ;
+        Node* next=nullptr;
+        //duplicate
+        while(temp){
+            next = temp->next;
+            Node* newNode = new Node( temp->val );
+            temp->next = newNode;
+            newNode->next = next;
+            temp=next;
+        } 
+
+        //set random pointer
         temp=head;
         while(temp){
-            if(temp->random)temp->next->random=temp->random->next;
-            temp=temp->next->next;
-
+            if(temp->random)temp->next->random = temp->random->next;
+            else temp->next->random = nullptr; 
+            temp = temp->next->next;
         }
 
-        //separate old and new lists
-        temp=head;
+        //separate lists
         Node* newHead=head->next;
+        Node* tail=nullptr;
+        Node* newTail=nullptr;
+        temp=head;
         while(temp){
-            Node* copy=temp->next;
-            temp->next=temp->next->next;
-            if(copy->next)copy->next=copy->next->next;
-            temp=temp->next;
-        }
+            next=temp->next->next;
+            if(!tail){
+                tail=temp;
+            }
+            else{
+                tail->next=temp;
+                tail=tail->next;
+            }
+
+            if(!newTail){
+                newTail=temp->next;
+            }
+            else{
+                newTail->next=temp->next;
+                newTail=newTail->next;
+            }
+            temp=next;
+        } 
+        tail->next=nullptr;
+        newTail->next=nullptr;
+
         return newHead;
 
 
